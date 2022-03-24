1d76bf4f31e7a96e6f0665f62cebaa01af21f74b
10thfloor <remember.mackenzie@gmail.com>
2022-03-24 14:27:28 -0700

    makes flags work.

--- pages/api/flags/local.js
+++ pages/api/flags/local.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ hello: "hello flags" });
+  res.status(200).json({ flags: { hello: "hello flags" } });
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import { useFlags } from "@happykit/flags/client";
 
 export default function Home() {
   const { flags } = useFlags();
+  console.log(flags);
   return (
     <div>
       <main>Hello Next Docs</main>
-      {flags && flags.isLocal && <p>Flagged: Local</p>}
+      {flags && flags.hello && <p>Flagged: Local</p>}
     </div>
   );
 }
