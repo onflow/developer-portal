5417125883bab221a7a58244794278e300abf962
10thfloor <remember.mackenzie@gmail.com>
2022-03-25 09:35:09 -0700

    Adds theme context.

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("flow-docs-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+
+    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userMedia.matches) {
+      return "dark";
+    }
+  }
+
+  return "dark";
+};
+
+export const ThemeContext = createContext({});
+
+export const ThemeProvider = ({ initialTheme = false, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const rawSetTheme = (rawTheme) => {
+    const root = window.document.documentElement;
+    const isDark = rawTheme === "dark";
+
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(rawTheme);
+
+    localStorage.setItem("flow-docs-theme", rawTheme);
+  };
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme);
+  }
+
+  useEffect(() => {
+    rawSetTheme(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
