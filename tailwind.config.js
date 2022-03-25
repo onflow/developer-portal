e82a99b0c4b835454a7cc9a0b183c9278df0bd67
10thfloor <remember.mackenzie@gmail.com>
2022-03-25 09:46:08 -0700

    Adds Tailwind plugins and cssnano

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,9 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio")
+  ]
 };
