9d020e501e4758aa26ad22eea3bbba16ca778b4f
10thfloor <remember.mackenzie@gmail.com>
2022-04-11 15:50:50 -0700

    Integrate UI library + CMS tools.

--- apps/flow-docs/app/routes/index.tsx
+++ apps/flow-docs/app/routes/index.tsx
@@ -2,6 +2,11 @@ import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
+import { TestComponent } from "@flow-docs/ui";
+import { cmsTools } from "@flow-docs/cms-tools";
+
+cmsTools();
+
 export default function Index() {
   const user = useOptionalUser();
   return (
@@ -11,27 +16,24 @@ export default function Index() {
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
             <div className="absolute inset-0">
               <img
-                className="h-full w-full object-cover"
+                className="object-cover w-full h-full"
                 src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
                 alt="Sonic Youth On Stage"
               />
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
             </div>
-            <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-yellow-500 drop-shadow-md">
+            <div className="relative px-4 pt-16 pb-8 lg:pb-18 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
+              <h1 className="text-6xl font-extrabold tracking-tight text-center sm:text-8xl lg:text-9xl">
+                <span className="block text-yellow-500 uppercase drop-shadow-md">
                   Indie Stack
                 </span>
               </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
-              </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+              <TestComponent />
+              <div className="max-w-sm mx-auto mt-10 sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
                     to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                    className="flex items-center justify-center px-4 py-3 text-base font-medium text-yellow-700 bg-white border border-transparent rounded-md shadow-sm hover:bg-yellow-50 sm:px-8"
                   >
                     View Notes for {user.email}
                   </Link>
@@ -39,13 +41,13 @@ export default function Index() {
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                      className="flex items-center justify-center px-4 py-3 text-base font-medium text-yellow-700 bg-white border border-transparent rounded-md shadow-sm hover:bg-yellow-50 sm:px-8"
                     >
                       Sign up
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
+                      className="flex items-center justify-center px-4 py-3 font-medium text-white bg-yellow-500 rounded-md hover:bg-yellow-600 "
                     >
                       Log In
                     </Link>
@@ -63,8 +65,8 @@ export default function Index() {
           </div>
         </div>
 
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-wrap justify-center gap-8">
+        <div className="px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex flex-wrap justify-center gap-8 mt-6">
             {[
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
@@ -125,7 +127,7 @@ export default function Index() {
               <a
                 key={img.href}
                 href={img.href}
-                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
+                className="flex justify-center w-32 h-16 p-1 transition grayscale hover:grayscale-0 focus:grayscale-0"
               >
                 <img alt={img.alt} src={img.src} />
               </a>
--- apps/flow-docs/test/setup-test-env.ts
+++ apps/flow-docs/test/setup-test-env.ts
@@ -1,4 +1,5 @@
 import { installGlobals } from "@remix-run/node/globals";
+
 import "@testing-library/jest-dom/extend-expect";
 
 installGlobals();
