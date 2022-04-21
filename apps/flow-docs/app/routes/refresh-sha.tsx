6b7a16effa340cb53f244940140c62c37ad98db3
10thfloor <remember.mackenzie@gmail.com>
2022-04-21 15:26:08 -0700

    Adds endpoint for fetching latest update sha from redis.

--- apps/flow-docs/app/routes/refresh-sha.tsx
+++ apps/flow-docs/app/routes/refresh-sha.tsx
@@ -3,9 +3,11 @@ import { redisCache } from "~/utils/cms/redis.server";
 import { commitShaKey as refreshCacheCommitShaKey } from "./action/refresh";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const shaInfo = await redisCache.get(
-    `${refreshCacheCommitShaKey}:${params["repo"]}`
-  );
+  const url = new URL(request.url);
+  const repo = url.searchParams.get("repo");
+
+  const shaInfo = await redisCache.get(`${refreshCacheCommitShaKey}:${repo}`);
+
   const data = JSON.stringify(shaInfo);
   return new Response(data, {
     headers: {
