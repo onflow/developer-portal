84f1fc5d3bce0bf95f801d5b7c24a794b7a3e328
Alex Serban <al.se@me.com>
2022-05-08 13:57:14 -0400

    Internal sidebar

--- libs/design-system/src/index.ts
+++ libs/design-system/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/another-component/another-component';
 export { Heading, HeadingProps } from './lib/Heading/Heading';
+export { InternalSidebar } from './lib/InternalSidebar/InternalSidebar';
 export { Link } from './lib/Link/Link';
 export * from './lib/test-component/test-component';
