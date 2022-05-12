a4ac2a173593771f530fadac8e4d199c362ed21d
Alex Serban <al.se@me.com>
2022-05-12 15:12:59 -0400

    Remove toc navlink

--- libs/design-system/src/lib/Components/InternalToc/index.tsx
+++ libs/design-system/src/lib/Components/InternalToc/index.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from '@remix-run/react';
 import clsx from 'clsx';
 import { MDXContentProps } from 'mdx-bundler/client';
 import { useEffect, useState } from 'react';
@@ -26,7 +25,9 @@ export function getHeadingsFromMdxComponent(
 
 export function InternalToc({ headings }: InternalTocProps) {
   const [hash, setHash] = useState('');
+  // TODO: pass location in props to remove react-router-dom dependency
   const location = useLocation();
+
   useEffect(() => {
     setHash(location.hash);
   }, [location.hash]);
@@ -41,8 +42,8 @@ export function InternalToc({ headings }: InternalTocProps) {
           const path = `#${id}`;
           return (
             <div className="flex" key={id}>
-              <NavLink
-                to={path}
+              <a
+                href={path}
                 className={clsx(
                   'mb-1 py-2 px-5 text-sm text-main-gray-400 hover:opacity-75 dark:text-gray-200',
                   {
@@ -52,7 +53,7 @@ export function InternalToc({ headings }: InternalTocProps) {
                 )}
               >
                 {value}
-              </NavLink>
+              </a>
             </div>
           );
         })}
