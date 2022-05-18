89c9ebd03f332e854650a77f1480a9a510e5147e
Alex Serban <al.se@me.com>
2022-05-17 21:43:25 -0400

    Lower page navigation component

---
+++ libs/design-system/src/lib/Components/LowerPageNav/index.tsx
@@ -0,0 +1,20 @@
+import { LowerPageNavLink } from './LowerPageNavLink';
+
+export type LowerPageNavLink = {
+  name: string;
+  href: string;
+};
+
+export type LowerPageNavProps = {
+  prev: LowerPageNavLink;
+  next: LowerPageNavLink;
+};
+
+export function LowerPageNav({ prev, next }: LowerPageNavProps) {
+  return (
+    <div className="flex flex-wrap flex-1 p-6 place-content-between">
+      {!!prev && <LowerPageNavLink link={prev} />}
+      {!!next && <LowerPageNavLink link={next} next={true} />}
+    </div>
+  );
+}
