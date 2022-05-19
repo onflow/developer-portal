e865d97b52b4ce63fe200c0dc298330b4ec40ee8
Hannah Lee <hannahlee@Hannahs-MacBook-Pro.local>
2022-05-19 15:16:11 -0400

    Replace docsearch secrets with testing values

--- libs/design-system/src/lib/Components/Search/index.tsx
+++ libs/design-system/src/lib/Components/Search/index.tsx
@@ -2,18 +2,20 @@ import { DocSearch } from '@docsearch/react';
 import '../../../../styles/docsearch.css';
 
 export type SearchProps = {
-  appId: string;
-  apiKey: string;
   indexName: string;
+  placeholder?: string;
 };
 
-export function Search({ appId, apiKey, indexName }: SearchProps) {
+export function Search({ placeholder, indexName }: SearchProps) {
+  const placeholderVal = placeholder ?? 'Search Documentation';
+  const indexNameVal = indexName ?? 'search-index';
   return (
+    /* TODO: Replace the testing credentials with real keys using .env and dotenv package */
     <DocSearch
-      appId="QE0LM9XKDG"
-      indexName="search-index"
-      apiKey="74700aee1bcb782c019adc7bc92fde31"
-      placeholder="Search Documentation..."
+      appId="R2IYF7ETH7" // Testing Credentials
+      apiKey="599cec31baffa4868cae4e79f180729b" // Testing Credentials
+      indexName={indexNameVal}
+      placeholder={placeholderVal}
     />
   );
 }
