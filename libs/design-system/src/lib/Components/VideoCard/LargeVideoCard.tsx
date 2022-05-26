220f2273444cb05607da08b121f5923a88c07f66
Neilzon Viloria <neilcviloria@gmail.com>
2022-05-26 15:08:12 -0600

    remove comment

--- libs/design-system/src/lib/Components/VideoCard/LargeVideoCard.tsx
+++ libs/design-system/src/lib/Components/VideoCard/LargeVideoCard.tsx
@@ -9,10 +9,6 @@ export interface LargeVideoCardProps {
   length: number; // seconds
 }
 
-/**
- * 
- * @param param0 
- */
 export function LargeVideoCard({ link, title, length }: LargeVideoCardProps) {
   const [showOverlay, setShowOverlay] = useState(true);
 
@@ -45,7 +41,6 @@ export function LargeVideoCard({ link, title, length }: LargeVideoCardProps) {
           <div className="pointer-events-none absolute grid h-full w-full place-items-center">
             <PlayCircle width={48} height={64} />
           </div>
-
           <div className="pointer-events-none absolute bottom-0 left-0 w-full p-6 text-white md:w-3/4">
             <div className="text-h4  line-clamp-2">{title}</div>
             <div className="items-align flex gap-2">
