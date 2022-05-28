67f10bfff94d908439fbc5b794bd4ee99e21364b
Cal Perez <aure.entuluva.cal@gmail.com>
2022-05-28 10:58:27 -0400

    Adjust closed state of sporkscard

--- libs/design-system/src/lib/Components/SporksCard/index.tsx
+++ libs/design-system/src/lib/Components/SporksCard/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns';
 import { ReactComponent as CopyIcon } from '../../../../images/action/copy.svg';
 import { ReactComponent as ChevronUpIcon } from '../../../../images/arrows/chevron-up.svg';
+import { ReactComponent as ChevronDownIcon } from '../../../../images/arrows/chevron-down.svg';
 
 export type SporkMetadata = {
   accessNode: string;
@@ -51,7 +52,7 @@ const Spork = ({ heading, timestamp, sporkMetadata }) => {
   const [isExpanded, setIsExpanded] = useState(true);
 
   return (
-    <div className="flex-col items-center justify-between px-2 py-4 bg-white rounded-2xl px-11 hover:shadow-2xl dark:bg-primary-dark-gray">
+    <div className="flex-col items-center justify-between px-4 py-6 bg-white rounded-2xl hover:shadow-2xl dark:bg-primary-dark-gray md:px-8">
       <div
         className="flex justify-between px-2 ease-in cursor-pointer"
         tabIndex={0}
@@ -60,17 +61,15 @@ const Spork = ({ heading, timestamp, sporkMetadata }) => {
         onClick={() => setIsExpanded(!isExpanded)}
       >
         <div className="flex items-center">
-          <span className="text-xl text-2xl font-bold">{heading}</span>
-          <span className="ml-12">{format(timestamp, 'MMMM d')}</span>
+          <span className="pr-4 text-xl text-2xl font-bold">{heading}</span>
+          <span className="pl-4 border-l border-primary-gray-100 text-primary-gray-300">
+            {format(timestamp, 'MMMM d')}
+          </span>
         </div>
-        {isExpanded && (
-          <div className="mt-2">
-            <ChevronUpIcon />
-          </div>
-        )}
+        {isExpanded ? <ChevronUpIcon /> : <ChevronDownIcon />}
       </div>
       {isExpanded && (
-        <div className="flex-col py-4">
+        <div className="flex-col pt-4 pb-2">
           <CardItem label="Access Node" data={accessNode} />
           <CardItem label="Date" data={format(date, 'LLL d, yyyy')} />
           <CardItem label="Root Height" data={rootHeight} />
@@ -87,15 +86,14 @@ const Spork = ({ heading, timestamp, sporkMetadata }) => {
 
 const UpcomingSpork = ({ heading, timestamp }) => {
   return (
-    <div className="flex-col items-center justify-between px-2 py-4 bg-white rounded-2xl px-11 dark:bg-primary-dark-gray">
-      <div className="flex justify-between px-2">
-        <div className="flex items-center">
-          <span className="text-xl text-2xl font-bold">{heading}</span>
-          <span className="ml-12">
-            Coming in {formatDistanceToNow(timestamp)} (
-            {format(timestamp, 'MMMM d')} 8-9AM PST)
-          </span>
-        </div>
+    <div className="flex-col items-center justify-between px-4 py-6 bg-white rounded-2xl dark:bg-primary-dark-gray md:px-8">
+      <div className="flex flex-col justify-start px-2 md:flex-row">
+        <span className="text-xl text-2xl font-bold md:pr-4">{heading}</span>
+        <hr className="inline-block w-6 my-4 md:hidden" />
+        <span className="border-l border-primary-gray-100 md:pl-4">
+          Coming in {formatDistanceToNow(timestamp)} (
+          {format(timestamp, 'MMMM d')} 8-9AM PST)
+        </span>
       </div>
     </div>
   );
