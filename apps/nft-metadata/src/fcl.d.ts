3f2784f2395235858cb5b57e65151849e74a2f82
Pratik Patel <prpatel05@gmail.com>
2022-05-31 12:42:50 -0400

    Integrate With cadence-to-json From MetadataViewsChecker.

--- apps/nft-metadata/src/fcl.d.ts
+++ apps/nft-metadata/src/fcl.d.ts
@@ -2,31 +2,33 @@
 // This is just a placeholder for when those come to fruition, and we can
 // plug it in here.
 
-declare module "@onflow/fcl" {
-  function currentUser(): any
-  function authenticate(): any
-  function getAccount(address: any): any
-  function account(address: any): any
-  function script(): any
-  function transaction(): any
-  function withPrefix(addr): any
-  function sansPrefix(addr): any
-  function config(args): any
-  function query(args): any
-  function send(args: any)
-  function script(args: any)
-  function decode()
-  function args(args): any
-  function arg(arg1, arg2): any
+declare module '@onflow/fcl' {
+  function currentUser(): any;
+  function authenticate(): any;
+  function getAccount(address: any): any;
+  function account(address: any): any;
+  function script(): any;
+  function transaction(): any;
+  function withPrefix(addr): any;
+  function sansPrefix(addr): any;
+  function config(args): any;
+  function query(args): any;
+  function send(args: any);
+  function script(args: any);
+  function decode();
+  function args(args): any;
+  function arg(arg1, arg2): any;
 }
 
-declare module "@onflow/types" {
+declare module '@onflow/types' {
   Address: string;
   String: string;
-  function Address(): any
-  function String(): any
+  Path: string;
+  function Address(): any;
+  function String(): any;
+  function Path(): any;
 }
 
-declare module "@onflow/transport-http" {
-  function send(): any
+declare module '@onflow/transport-http' {
+  function send(): any;
 }
--- apps/nft-metadata/src/flow/setup.tsx
+++ apps/nft-metadata/src/flow/setup.tsx
@@ -1,8 +1,42 @@
 import { config } from "@onflow/fcl";
 import { send as httpSend } from "@onflow/transport-http";
+import * as json from "./c2j.json";
 
-config({
-  "accessNode.api": "https://rest-testnet.onflow.org",
-  "discovery.wallet": "https://staging.accounts.meetdapper.com/fcl/authn-restricted",
-  "sdk.transport": httpSend
-})
+if (process.env["FLOW_ENVIRONMENT"] === "mainnet") {
+  // TODO: Set correct endpoints.
+  config({
+    "accessNode.api": "https://rest-testnet.onflow.org",
+    "discovery.wallet": "https://staging.accounts.meetdapper.com/fcl/authn-restricted",
+    "sdk.transport": httpSend
+  })
+  Object.keys(json.vars["mainnet"]).forEach(
+    (contractAddressKey) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config.put(
+            contractAddressKey,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            json.vars["mainnet"][contractAddressKey]
+        );
+    }
+  );
+} else {
+  config({
+    "accessNode.api": "https://rest-testnet.onflow.org",
+    "discovery.wallet": "https://staging.accounts.meetdapper.com/fcl/authn-restricted",
+    "sdk.transport": httpSend
+  })
+  Object.keys(json.vars["testnet"]).forEach(
+    (contractAddressKey) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        config.put(
+            contractAddressKey,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            json.vars["testnet"][contractAddressKey]
+        );
+    }
+  );
+}
