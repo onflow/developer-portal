a5ec0b45c98a3e8eecfcc54c1f9da5888d3e36dd
Cal Perez <aure.entuluva.cal@gmail.com>
2022-06-01 09:27:45 -0400

    Wrap up card

--- libs/design-system/src/lib/Components/FeaturedArticleCard/index.tsx
+++ libs/design-system/src/lib/Components/FeaturedArticleCard/index.tsx
@@ -18,13 +18,17 @@ const FeaturedArticleCard: React.FC<FeaturedArticleCardProps> = ({
   ctaText,
 }) => {
   return (
-    <div className="rounded-lg bg-white py-[122px] px-[80px] dark:bg-primary-gray-dark">
+    <div className="rounded-lg bg-white px-8 py-12 dark:bg-primary-gray-dark md:py-[122px] md:px-[80px]">
       {tags.map((tag) => (
         <Tag name={tag} />
       ))}
-      <div>{heading}</div>
-      <div>{description}</div>
-      <ButtonLink href={link}>{ctaText}</ButtonLink>
+      <div className="my-2 text-h2">{heading}</div>
+      <p className="mb-14 text-primary-gray-300 dark:text-primary-gray-200">
+        {description}
+      </p>
+      <ButtonLink href={link} className="px-6 py-4">
+        {ctaText}
+      </ButtonLink>
     </div>
   );
 };
