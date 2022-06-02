b740d67f80b792d470d5ec664dece67187137389
Neilzon Viloria <neilcviloria@gmail.com>
2022-06-02 13:09:28 -0600

    spork faq page layout

--- libs/design-system/src/lib/Pages/SporkFAQPage/index.tsx
+++ libs/design-system/src/lib/Pages/SporkFAQPage/index.tsx
@@ -31,35 +31,32 @@ const faqList = [
 
 export function SporkFAQPage() {
   return (
-    <div className="bg-zinc-50 dark:bg-black md:bg-white">
-      <InternalSubnav current="Spork FAQ" />
-      <main className="px-4 pb-48 md:px-0 md:pt-16">
-        <div className="grid grid-cols-1 items-start pb-6 md:flex md:pb-12">
-          <div className="order-2 grow">
-            <h1 className="text-h1 font-termina pb-14 font-bold leading-snug md:pb-6 md:font-sans md:text-4xl">
-              <span className="hidden md:block">Spork FAQ</span>
-              <span className="block md:hidden">FAQ</span>
-            </h1>
-            <Attribution
-              updatedDate="23/3/2022"
-              authorIcon="https://avatars.githubusercontent.com/u/62387156?s=64&v=4"
-              authorName="@maxxP"
-              otherAuthorsCount={12}
-              readMinutes={4}
-              difficulty="Beginners"
-            />
-          </div>
-          <div className="order-1 md:order-2">
-            <button className="mt-3 mb-2 flex items-center rounded-lg border-primary-blue py-4 text-primary-blue dark:border-primary-purple dark:text-primary-purple md:m-0 md:border md:px-5">
-              <span>
-                <LeftChevron className="stroke-primary-blue dark:stroke-primary-purple" />
-              </span>
-              Back to Network
-            </button>
-          </div>
+    <main className="bg-zinc-50 px-4 pb-48 dark:bg-black md:bg-white md:px-0 md:pt-16">
+      <div className="grid grid-cols-1 items-start pb-6 md:flex md:pb-12">
+        <div className="order-2 grow">
+          <h1 className="pb-14 font-display text-[44px] font-bold leading-snug md:pb-6 md:font-sans md:text-4xl">
+            <span className="hidden md:block">Spork FAQ</span>
+            <span className="block md:hidden">FAQ</span>
+          </h1>
+          <Attribution
+            updatedDate="23/3/2022"
+            authorIcon="https://avatars.githubusercontent.com/u/62387156?s=64&v=4"
+            authorName="@maxxP"
+            otherAuthorsCount={12}
+            readMinutes={4}
+            difficulty="Beginners"
+          />
         </div>
-        <FAQ faqList={faqList} variation="large" />
-      </main>
-    </div>
+        <div className="order-1 md:order-2">
+          <button className="mt-2 flex items-center rounded-lg border-primary-blue py-4 text-primary-blue dark:border-primary-purple dark:text-primary-purple md:m-0 md:border md:px-5">
+            <span>
+              <LeftChevron className="stroke-primary-blue dark:stroke-primary-purple" />
+            </span>
+            Back to Network
+          </button>
+        </div>
+      </div>
+      <FAQ faqList={faqList} variation="large" />
+    </main>
   );
 }
