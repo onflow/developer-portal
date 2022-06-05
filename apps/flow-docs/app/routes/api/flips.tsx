2cdc977da9ad255bc91dd8a6f260429fe17f5213
Hannah Lee <hannah6249@gmail.com>
2022-06-05 18:32:06 -0400

    use github api to pull FLIP pull request information instead of using hardcoded json files

--- apps/flow-docs/app/routes/api/flips.tsx
+++ apps/flow-docs/app/routes/api/flips.tsx
@@ -1,13 +1,23 @@
 import type { LoaderFunction } from "@remix-run/node";
-import flipSampleOutput from "./flipSampleOutput.json";
 import { useLoaderData } from "@remix-run/react";
 import type { FlipCellProps } from "../../../../../libs/design-system/dist/lib/Components/Flips/FlipCell";
 export type LoaderData = FlipCellProps[];
+import { Octokit } from "@octokit/core";
 
 export const loader: LoaderFunction = async () => {
-  // Fetch PRs from onflow/flow repo
-  // Filter only PRs with 'Flip' labels
-  const flipInfo = flipSampleOutput.filter((pr) =>
+  const octokit = new Octokit({
+    auth: process.env.PERSONAL_ACCESS_TOKEN,
+  });
+
+  const pullRequestResponse = await octokit
+    .request("GET /repos/{owner}/{repo}/pulls", {
+      owner: "onflow",
+      repo: "flow",
+    })
+    .then((response) => response.data);
+
+  // Fetch PRs from onflow/flow repo, then filter only PRs with 'Flip' labels
+  const flipInfo = pullRequestResponse.filter((pr) =>
     pr.labels.filter((label) => label.name == "FLIP")
   );
 
@@ -15,8 +25,11 @@ export const loader: LoaderFunction = async () => {
   const flipCellProps: LoaderData = flipInfo.map((pr) => ({
     numComments: 0, // TODO: We need to create another API call for this
     heading: pr.title,
-    tags: pr.labels.map((label) => label.name),
-    participant: { profileImage: pr.user.avatar_url, name: pr.user.login },
+    tags: pr.labels.map((label) => label.name ?? ""), // TODO: fix null assertions
+    participant: {
+      profileImage: pr.user?.avatar_url ?? "", // TODO: fix null assertions
+      name: pr.user?.login ?? "",
+    },
     date: pr.created_at,
     forumLink: pr.html_url,
   }));
@@ -26,7 +39,6 @@ export const loader: LoaderFunction = async () => {
 
 const FLIPs = () => {
   const flips = useLoaderData<LoaderData>();
-  console.log("Logging flips", { flips });
   return (
     <div>
       <h1>FLIPS</h1>
