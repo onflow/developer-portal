d1605c61a590d89be1b23cf0916f470895b84671
Joe Krill <joe@joekrill.com>
2022-06-06 15:47:36 -0400

    Prvent Pagination navigating to invalid pages

--- libs/design-system/src/lib/Components/Pagination/index.tsx
+++ libs/design-system/src/lib/Components/Pagination/index.tsx
@@ -1,4 +1,4 @@
- 
+import clsx from 'clsx';
 import { ReactComponent as ArrowLeftIcon } from '../../../../images/arrows/arrow-left.svg';
 import { ReactComponent as ArrowRightIcon } from '../../../../images/arrows/arrow-right.svg';
 
@@ -6,7 +6,7 @@ export type PaginationProps = {
   page: number;
   itemCount: number;
   pageSize: number;
-  setPage: (page: number) => null;
+  setPage: (page: number) => void;
 };
 
 const Pagination = ({
@@ -18,33 +18,47 @@ const Pagination = ({
   const pageCount = Math.ceil(itemCount / pageSize);
 
   return (
-    <div className="flex items-center justify-end text-primary-gray-300">
-      <div
-        role="button"
-        className="hover:cursor-pointer"
+    <div className="flex items-center justify-end ">
+      <button
+        type="button"
+        className={clsx('py-4 px-2', {
+          'cursor-pointer text-primary-gray-300 dark:text-white': page > 1,
+          'cursor-not-allowed text-primary-gray-100 dark:text-primary-gray-300':
+            page <= 1,
+        })}
+        disabled={page <= 1}
         onClick={() => setPage(page - 1)}
       >
         <ArrowLeftIcon />
-      </div>
+      </button>
       <input
         type="text"
-        className="w-10 ml-4 mr-2 text-center border rounded-sm border-primary-gray-300 dark:bg-black dark:text-white"
+        className="ml-4 mr-2 w-10 rounded-sm border border-primary-gray-300 text-center dark:bg-black dark:text-white"
         value={page}
         onChange={(e) => {
-          const {
-            target: { value },
-          } = e;
-          setPage(+value);
+          const value = +e.target.value;
+
+          if (!Number.isFinite(value) || value < 1 || value > pageCount) {
+            return;
+          }
+
+          setPage(value);
         }}
       />
       of {pageCount}
-      <div
-        role="button"
-        className="ml-4 hover:cursor-pointer"
+      <button
+        type="button"
+        className={clsx('ml-4 py-4 px-2', {
+          'cursor-pointer text-primary-gray-300 dark:text-white':
+            page < pageCount,
+          'cursor-not-allowed text-primary-gray-100 dark:text-primary-gray-300':
+            page >= pageCount,
+        })}
+        disabled={page >= pageCount}
         onClick={() => setPage(page + 1)}
       >
         <ArrowRightIcon />
-      </div>
+      </button>
     </div>
   );
 };
