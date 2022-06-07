b25a76e224ee63bc6da530185e30daf05d3dcd2e
Neilzon Viloria <neilcviloria@gmail.com>
2022-06-07 14:43:41 -0600

    change to button links

--- libs/design-system/src/lib/Pages/GettingStartedPage/index.tsx
+++ libs/design-system/src/lib/Pages/GettingStartedPage/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {
   LandingHeader,
   LandingHeaderProps,
@@ -25,6 +23,8 @@ import {
 } from '../../Components/LinkCard3Column';
 import { ReactComponent as GitHubIcon } from '../../../../images/social/github.svg';
 import { ReactComponent as DiscordIcon } from '../../../../images/social/discord.svg';
+
+
 
 export interface GettingStartedPageProps {
   landingHeaderItems: LandingHeaderProps;
@@ -99,9 +99,9 @@ export function GettingStartedPage({
         <div className="container">
           <div className="mb-9 flex items-end justify-between">
             <h2 className="text-h2">SDK's</h2>
-            <Button next variant="secondary" className="hidden md:inline-flex">
+            <ButtonLink next variant="secondary" className="hidden md:inline-flex" href='#'>
               View All SDKs
-            </Button>
+            </ButtonLink>
           </div>
           <div className="mb-4 grid grid-cols-1 grid-rows-5 gap-4 md:grid-cols-2 md:grid-rows-3">
             {sdkCardItems.map((sdkCard, i) => (
@@ -119,9 +119,9 @@ export function GettingStartedPage({
               />
             ))}
           </div>
-          <Button variant="primary" className="w-full md:hidden">
+          <ButtonLink variant="primary" className="w-full md:hidden" href='#'>
             View All SDKs
-          </Button>
+          </ButtonLink>
         </div>
       </div>
 
@@ -133,9 +133,9 @@ export function GettingStartedPage({
 
           <div className="flex items-end justify-between md:items-center ">
             <h4 className="text-h4">Recent Tools</h4>
-            <Button next variant="secondary" className="hidden md:inline-flex">
+            <ButtonLink next variant="secondary" className="hidden md:inline-flex" href='#'>
               View All Tools
-            </Button>
+            </ButtonLink>
           </div>
 
           <div className="hidden md:block">
