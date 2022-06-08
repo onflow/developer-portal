24d7b13b8e4e96100e075379711d519aa592469d
Peter Schilling <peter@tanookilabs.com>
2022-06-07 18:17:17 -0700

    Switchup rollup config

--- libs/design-system/rollup.config.js
+++ libs/design-system/rollup.config.js
@@ -1,18 +1,15 @@
-
 const peerDepsExternal = require('rollup-plugin-peer-deps-external');
 const url = require('@rollup/plugin-url');
 const svgr = require('@svgr/rollup');
+const typescript = require('@rollup/plugin-typescript');
 
 module.exports = (options) => ({
   ...options,
   plugins: [
+    peerDepsExternal(),
     ...options.plugins,
+    typescript({ tsconfig: './libs/design-system/tsconfig.json' }),
     url(),
     svgr(),
-    peerDepsExternal({
-      includeDependencies: true,
-      dedupe: ['react', 'react-dom']
-    })
   ],
-
 });
