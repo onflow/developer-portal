2b0e5e4dc95c662eeb61ec2830074a57d0e1cf8d
Alex Serban <al.se@me.com>
2022-06-10 16:14:01 -0400

    Run Prettier

--- app/ui/design-system/src/lib/Components/ToolCard/ToolCard.stories.tsx
+++ app/ui/design-system/src/lib/Components/ToolCard/ToolCard.stories.tsx
@@ -1,31 +1,31 @@
-import { Meta, Story } from '@storybook/react';
+import { Meta, Story } from "@storybook/react"
 
-import { ReactComponent as CadenceIcon } from '../../../../images/tools/tool-cadence';
-import { ToolCard, ToolCardProps } from '.';
+import { ReactComponent as CadenceIcon } from "../../../../images/tools/tool-cadence"
+import { ToolCard, ToolCardProps } from "."
 
 export default {
   component: ToolCard,
-  title: 'Components/ToolCard',
-} as Meta;
+  title: "Components/ToolCard",
+} as Meta
 
 const Template: Story<ToolCardProps> = (args) => {
   return (
     <div className="bg-gray-100 p-4 dark:bg-black">
       <ToolCard {...args} />
     </div>
-  );
-};
+  )
+}
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-  title: 'Flow Port',
-  authorIcon: 'https://avatars.githubusercontent.com/u/62387156?s=64&v=4',
-  authorName: 'mini flow',
-  tags: ['Tags'],
-  link: '#',
-  type: 'tool',
+  title: "Flow Port",
+  authorIcon: "https://avatars.githubusercontent.com/u/62387156?s=64&v=4",
+  authorName: "mini flow",
+  tags: ["Tags"],
+  link: "#",
+  type: "tool",
   stars: 52,
   toolIcon: <CadenceIcon />,
   description:
-    'Lorem ipsum text here can go a two liner sentence or a one liner',
-};
+    "Lorem ipsum text here can go a two liner sentence or a one liner",
+}
--- app/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories.tsx
+++ app/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories.tsx
@@ -1,112 +1,112 @@
-import { Meta, Story } from '@storybook/react';
-import { UpcomingEvents, UpcomingEventsProps } from '.';
+import { Meta, Story } from "@storybook/react"
+import { UpcomingEvents, UpcomingEventsProps } from "."
 
 export default {
   component: UpcomingEvents,
-  title: 'Components/UpcomingEvents',
-} as Meta;
+  title: "Components/UpcomingEvents",
+} as Meta
 
 const Template: Story<UpcomingEventsProps> = (args) => (
-  <div className="bg-primary-gray-50 dark:bg-black" style={{ padding: '14px' }}>
+  <div className="bg-primary-gray-50 dark:bg-black" style={{ padding: "14px" }}>
     <UpcomingEvents {...args} />
   </div>
-);
+)
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
   officeHours: <p>[office hours placeholder]</p>,
   workingHours: <p>[working hours placeholder]</p>,
-  goToCommunityHref: '#todo',
-  submitEventHref: '#todo',
+  goToCommunityHref: "#todo",
+  submitEventHref: "#todo",
   primaryEvents: [
     {
-      ctaText: 'CTA Here',
+      ctaText: "CTA Here",
       description:
-        'Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow',
-      eventDate: 'Mar 23',
-      href: '#todo',
+        "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+      eventDate: "Mar 23",
+      href: "#todo",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png',
-      location: 'Online',
-      tags: ['Flow official'],
-      title: 'Event Title',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+      location: "Online",
+      tags: ["Flow official"],
+      title: "Event Title",
     },
     {
-      ctaText: 'CTA Here',
+      ctaText: "CTA Here",
       description:
-        'Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow',
-      eventDate: 'Mar 23',
-      href: '#todo',
+        "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+      eventDate: "Mar 23",
+      href: "#todo",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png',
-      location: 'Online',
-      tags: ['Flow official'],
-      title: 'Event Title',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+      location: "Online",
+      tags: ["Flow official"],
+      title: "Event Title",
     },
     {
-      ctaText: 'CTA Here',
+      ctaText: "CTA Here",
       description:
-        'Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow',
-      eventDate: 'Mar 23',
-      href: '#todo',
+        "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+      eventDate: "Mar 23",
+      href: "#todo",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png',
-      location: 'Online',
-      tags: ['Flow official'],
-      title: 'Event Title',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+      location: "Online",
+      tags: ["Flow official"],
+      title: "Event Title",
     },
   ],
   secondaryEvents: [
     {
-      href: 'https://www.onflow.org',
-      eventType: 'Online',
+      href: "https://www.onflow.org",
+      eventType: "Online",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png',
-      tags: ['Flow official'],
-      title: 'FLIP contest',
-      when: 'May 5th, 5pm',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      tags: ["Flow official"],
+      title: "FLIP contest",
+      when: "May 5th, 5pm",
     },
     {
-      href: 'https://www.onflow.org',
-      eventType: 'Online',
+      href: "https://www.onflow.org",
+      eventType: "Online",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png',
-      tags: ['Flow official'],
-      title: 'FLIP contest',
-      when: 'May 5th, 5pm',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      tags: ["Flow official"],
+      title: "FLIP contest",
+      when: "May 5th, 5pm",
     },
     {
-      href: 'https://www.onflow.org',
-      eventType: 'Online',
+      href: "https://www.onflow.org",
+      eventType: "Online",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png',
-      tags: ['Flow official'],
-      title: 'FLIP contest',
-      when: 'May 5th, 5pm',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      tags: ["Flow official"],
+      title: "FLIP contest",
+      when: "May 5th, 5pm",
     },
     {
-      href: 'https://www.onflow.org',
-      eventType: 'Online',
+      href: "https://www.onflow.org",
+      eventType: "Online",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png',
-      tags: ['Flow official'],
-      title: 'FLIP contest',
-      when: 'May 5th, 5pm',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      tags: ["Flow official"],
+      title: "FLIP contest",
+      when: "May 5th, 5pm",
     },
     {
-      href: 'https://www.onflow.org',
-      eventType: 'Online',
+      href: "https://www.onflow.org",
+      eventType: "Online",
       imageSrc:
-        'https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png',
-      tags: ['Flow official'],
-      title: 'FLIP contest',
-      when: 'May 5th, 5pm',
+        "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      tags: ["Flow official"],
+      title: "FLIP contest",
+      when: "May 5th, 5pm",
     },
   ],
-};
+}
 
-export const SingleEvent = Template.bind({});
+export const SingleEvent = Template.bind({})
 SingleEvent.args = {
   ...Default.args,
   primaryEvents: Default.args.primaryEvents.slice(0, 1),
-};
+}
