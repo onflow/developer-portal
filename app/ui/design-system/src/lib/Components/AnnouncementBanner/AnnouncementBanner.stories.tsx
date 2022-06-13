6b320f2757a932d45ff278f5028f4aaa9dcbf1f7
Neilzon Viloria <neilcviloria@gmail.com>
2022-06-13 14:26:30 -0600

    styling

--- app/ui/design-system/src/lib/Components/AnnouncementBanner/AnnouncementBanner.stories.tsx
+++ app/ui/design-system/src/lib/Components/AnnouncementBanner/AnnouncementBanner.stories.tsx
@@ -1,10 +1,10 @@
-import { Meta, Story } from "@storybook/react";
-import { AnnouncementBanner } from ".";
+import { Meta, Story } from "@storybook/react"
+import { AnnouncementBanner } from "."
 
 export default {
   component: AnnouncementBanner,
   title: "Components/AnnouncementBanner",
-} as Meta;
+} as Meta
 
 const Template: Story = (args) => {
   return (
@@ -12,20 +12,24 @@ const Template: Story = (args) => {
       <div>
         Permissionless deployment is coming to Flow! Read more{" "}
         <span style={{ textDecoration: "underline" }}>
-          <a href="https://permissionless.onflow.org/" target="_blank">
+          <a
+            href="https://permissionless.onflow.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
             here
           </a>
         </span>
       </div>
     </AnnouncementBanner>
-  );
-};
+  )
+}
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 
-export const Mobile = Template.bind({});
+export const Mobile = Template.bind({})
 Mobile.parameters = {
   viewport: {
     defaultViewport: "xs",
   },
-};
+}
