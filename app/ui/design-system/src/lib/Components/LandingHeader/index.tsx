45b8f9009853619407685dbcb18c8aa8bd186e3f
Cal Perez <aure.entuluva.cal@gmail.com>
2022-06-14 15:45:54 -0400

    Add new section

--- app/ui/design-system/src/lib/Components/LandingHeader/index.tsx
+++ app/ui/design-system/src/lib/Components/LandingHeader/index.tsx
@@ -1,5 +1,8 @@
 import { ButtonLink } from "../Button"
 import { GradientName, LandingHeaderContainer } from "./LandingHeaderContainer"
+import { ReactComponent as DiscordIcon } from "../../../../images/social/discord"
+import { ReactComponent as GithubIcon } from "../../../../images/social/github"
+import { GITHUB_URL, DISCORD_URL } from "../../constants"
 
 export type LandingHeaderProps = {
   buttonText: string
@@ -20,23 +23,33 @@ export function LandingHeader({
 }: LandingHeaderProps) {
   return (
     <LandingHeaderContainer gradient={gradient}>
-      <div className="container mx-auto flex flex-col justify-items-stretch pt-16 md:pt-20">
-        <h1 className="text-h1 mb-14 max-w-full overflow-hidden text-ellipsis !text-4xl md:mt-24 md:!text-7xl md:!leading-tight">
-          {title}
-        </h1>
-        <div className="flex flex-row items-stretch rounded-lg bg-primary-gray-100/30">
-          <div className="px-5 py-10 md:basis-1/2 md:px-20 md:py-12">
-            <h2 className="text-h2 mb-2 md:mb-4">{callout}</h2>
-            <p>{description}</p>
-            <ButtonLink
-              className="mt-4 mb-4 md:mt-12"
-              href={buttonUrl}
-              variant="primary-no-darkmode"
-            >
-              {buttonText}
-            </ButtonLink>
+      <div className="flex">
+        <div className="container mx-auto flex flex-col justify-items-stretch pt-16 md:pt-20">
+          <h1 className="text-h1 mb-14 max-w-full overflow-hidden text-ellipsis !text-4xl md:mt-24 md:!text-7xl md:!leading-tight">
+            {title}
+          </h1>
+          <div className="flex flex-row items-stretch rounded-lg bg-primary-gray-100/30">
+            <div className="px-5 py-10 md:basis-1/2 md:px-20 md:py-12">
+              <h2 className="text-h2 mb-2 md:mb-4">{callout}</h2>
+              <p>{description}</p>
+              <ButtonLink
+                className="mt-4 mb-4 md:mt-12"
+                href={buttonUrl}
+                variant="primary-no-darkmode"
+              >
+                {buttonText}
+              </ButtonLink>
+            </div>
+            <div className="hidden basis-1/2 rounded-r-lg bg-white px-10 py-6 dark:bg-white/40 md:block md:px-20 md:py-12"></div>
           </div>
-          <div className="hidden basis-1/2 rounded-r-lg bg-white px-10 py-6 dark:bg-white/40 md:block md:px-20 md:py-12"></div>
+        </div>
+        <div className="fixed top-1/2 hidden flex-col text-center text-primary-gray-400 dark:text-white md:flex">
+          <a href={DISCORD_URL} className="pb-4">
+            <DiscordIcon />
+          </a>
+          <a href={GITHUB_URL}>
+            <GithubIcon />
+          </a>
         </div>
       </div>
     </LandingHeaderContainer>
--- app/ui/design-system/src/lib/Components/LandingHeaderHome/index.tsx
+++ app/ui/design-system/src/lib/Components/LandingHeaderHome/index.tsx
@@ -1,5 +1,8 @@
 import LandingImage from "../../../../images/misc/landing-home.png"
 import { LandingHeaderContainer } from "../LandingHeader/LandingHeaderContainer"
+import { ReactComponent as DiscordIcon } from "../../../../images/social/discord"
+import { ReactComponent as GithubIcon } from "../../../../images/social/github"
+import { GITHUB_URL, DISCORD_URL } from "../../constants"
 
 export type LandingHeaderHomeProps = {
   description: string
@@ -14,22 +17,32 @@ export function LandingHeaderHome({
 }: LandingHeaderHomeProps) {
   return (
     <LandingHeaderContainer className="flex justify-center" gradient="home">
-      <div className="container flex flex-col items-center md:flex-row">
-        <div className="flex max-w-full flex-1 basis-1/2 flex-col items-start self-start py-6 pr-4 md:mt-24 md:py-20">
-          <p className="mb-1 font-display font-bold text-primary-gray-300 dark:text-primary-gray-200">
-            #{tag}
-          </p>
-          <h1 className="text-h1 mb-6 max-w-full overflow-hidden text-ellipsis !text-4xl md:!text-7xl ">
-            {title}
-          </h1>
-          <p>{description}</p>
+      <div className="flex">
+        <div className="container flex flex-col items-center md:flex-row">
+          <div className="flex max-w-full flex-1 basis-1/2 flex-col items-start self-start py-6 pr-4 md:mt-24 md:py-20">
+            <p className="mb-1 font-display font-bold text-primary-gray-300 dark:text-primary-gray-200">
+              #{tag}
+            </p>
+            <h1 className="text-h1 mb-6 max-w-full overflow-hidden text-ellipsis !text-4xl md:!text-7xl ">
+              {title}
+            </h1>
+            <p>{description}</p>
+          </div>
+          <div className="order-first flex-1 basis-1/2 md:order-last">
+            <img
+              src={LandingImage}
+              alt=""
+              className="max-h-[195px] object-cover md:max-h-[540px]"
+            />
+          </div>
         </div>
-        <div className="order-first flex-1 basis-1/2 md:order-last">
-          <img
-            src={LandingImage}
-            alt=""
-            className="max-h-[195px] object-cover md:max-h-[540px]"
-          />
+        <div className="fixed top-1/2 hidden flex-col text-center text-primary-gray-400 dark:text-white md:flex">
+          <a href={DISCORD_URL} className="pb-4">
+            <DiscordIcon />
+          </a>
+          <a href={GITHUB_URL}>
+            <GithubIcon />
+          </a>
         </div>
       </div>
     </LandingHeaderContainer>
