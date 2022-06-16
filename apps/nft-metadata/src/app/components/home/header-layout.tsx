3db327c5632cb4b8df0f85fbace4920d99520d75
Bilal Shahid <bshahid331@gmail.com>
2022-06-16 13:43:36 -0400

    Bilal/nft metadata (#186)
    Change content on home page

--- apps/nft-metadata/src/app/components/home/header-layout.tsx
+++ apps/nft-metadata/src/app/components/home/header-layout.tsx
@@ -2,6 +2,6 @@ import { LandingHeaderHome } from '@nft-metadata/ui';
 
 export function HeaderLayout() {
   return (<section className="lg:px-12">
-    <LandingHeaderHome title='NFT Portal' description='Browse NFT Collections and their available metadata. Verify your NFT collection is well-formed, and have your NFT added to the catalog for automatic support on major platforms.' tag='onFlow' />
+    <LandingHeaderHome title='NFT Portal' description='Browse NFT Collections and their available metadata. Verify your NFT collection is well-formed, and have your NFT added to the catalog to unlock interoperability throughout the Flow ecosystem.' tag='onFlow' />
   </section>);
 }
