1ea639854ec5851e9ecd05a706c6ce00ef384985
Alex Serban <al.se@me.com>
2022-06-16 15:40:02 -0400

    Add community page to remix app

--- app/routes/community/index.tsx
+++ app/routes/community/index.tsx
@@ -1,9 +1,56 @@
+import { useLoaderData } from "@remix-run/react"
 import { LoaderFunction } from "@remix-run/server-runtime"
+import { fetchOpenFlips } from "~/cms/utils/fetch-flips"
+import { CommunityMembersProps } from "~/ui/design-system/src/lib/Components/CommunityMembers"
+import { Default as DefaultCommunityMembers } from "~/ui/design-system/src/lib/Components/CommunityMembers/CommunityMembers.stories"
+import { ForumCellProps } from "~/ui/design-system/src/lib/Components/ForumCell"
+import { Default as DefaultForumCell } from "~/ui/design-system/src/lib/Components/ForumCell/ForumCell.stories"
+import { UpcomingEventsProps } from "~/ui/design-system/src/lib/Components/UpcomingEvents"
+import { Default as DefaultUpcomingEvents } from "~/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories"
+import CommunityPage, {
+  CommunityPageProps,
+} from "~/ui/design-system/src/lib/Pages/CommunityPage"
+import { articles, contentNavigationItems, projects, tools } from "./data"
+
+type DynamicCommunityPageProps = Pick<
+  CommunityPageProps,
+  "flips" | "communityMembers" | "concepts" | "upcomingEvents" | "forumTopics"
+>
 
 export const loader: LoaderFunction = async () => {
-  return {}
+  const flips = await fetchOpenFlips()
+  const upcomingEvents = DefaultUpcomingEvents?.args as UpcomingEventsProps
+  const forumTopics = [
+    DefaultForumCell?.args,
+    DefaultForumCell?.args,
+    DefaultForumCell?.args,
+    DefaultForumCell?.args,
+  ] as ForumCellProps[]
+  const communityMembers =
+    DefaultCommunityMembers?.args as CommunityMembersProps
+  const data: DynamicCommunityPageProps = {
+    flips,
+    communityMembers,
+    upcomingEvents,
+    forumTopics,
+  }
+  return data
 }
 
 export default function Page() {
-  return <div>Community Homepage</div>
+  const { flips, communityMembers, upcomingEvents, forumTopics } =
+    useLoaderData<DynamicCommunityPageProps>()
+
+  return (
+    <CommunityPage
+      flips={flips}
+      communityMembers={communityMembers}
+      projects={projects}
+      upcomingEvents={upcomingEvents}
+      articles={articles}
+      tools={tools}
+      contentNavigationItems={contentNavigationItems}
+      forumTopics={forumTopics}
+    />
+  )
 }
