022bc9149b7bae10b56e690dccd873f77fa7c33b
Hannah Lee <hannah6249@gmail.com>
2022-06-17 17:38:09 -0400

    add category as heading

--- app/cms/utils/fetch-discourse-api.tsx
+++ app/cms/utils/fetch-discourse-api.tsx
@@ -61,6 +61,64 @@ export interface Topic {
   featured_link: string
   posters: any
 }
+
+interface CategoryResponse {
+  category: Category
+}
+
+interface Category {
+  id: number
+  name: string
+  color: string
+  text_color: string
+  slug: string
+  topic_count: number
+  post_count: number
+  position: number
+  description: string
+  description_text: string
+  description_excerpt: string
+  topic_url: string
+  read_restricted: boolean
+  permission: number
+  notification_level: number
+  can_edit: boolean
+  topic_template: string
+  has_children: string
+  sort_order: string
+  sort_ascending: string
+  show_subcategory_list: boolean
+  num_featured_topics: number
+  default_view: string
+  subcategory_list_style: string
+  default_top_period: string
+  default_list_filter: string
+  minimum_required_tags: number
+  navigate_to_first_post_after_read: boolean
+  custom_fields: any
+  allowed_tags: any[]
+  allowed_tag_groups: any[]
+  allow_global_tags: boolean
+  required_tag_groups: any
+  read_only_banner: string
+  available_groups: any[]
+  auto_close_hours: string
+  auto_close_based_on_last_post: boolean
+  allow_unlimited_owner_edits_on_first_post: boolean
+  default_slow_mode_seconds: string
+  group_permissions: any
+  email_in: string
+  email_in_allow_strangers: boolean
+  mailinglist_mirror: boolean
+  all_topics_wiki: boolean
+  can_delete: boolean
+  allow_badges: boolean
+  topic_featured_link_allowed: boolean
+  search_priority: number
+  uploaded_logo: string
+  uploaded_background: string
+}
+
 const createForumLink = (
   topicSlug: string,
   topicId: number,
@@ -82,25 +140,43 @@ const convertToUser = (username: string, displayUrl: string) =>
     name: username,
   } as User)
 
-const convertTopicToForumCellProps = (topic: Topic) =>
+const convertTopicToForumCellProps = async (topic: Topic) =>
   ({
     numComments: topic.highest_post_number - 1,
     heading: topic.fancy_title,
-    subheading: "", // TODO: what is subheading? category?
+    subheading: await fetchCategoryById(topic.category_id),
     participants: [convertToUser(topic.last_poster_username, topic.image_url)],
     lastUpdatedDate: topic.last_posted_at, // "YYYY-MM-DDTHH:MM:SS.XXXZ"
     forumLink: createForumLink(topic.slug, topic.id, topic.highest_post_number),
   } as ForumCellProps)
 
-const formatLatestFiveTopics = (topics: Topic[]) =>
-  topics.slice(0, 5).map((topic) => convertTopicToForumCellProps(topic))
+const formatLatestFiveTopics = async (topics: Topic[]) => {
+  const fiveTopics = topics.slice(0, 5)
+  const formattedTopics = fiveTopics.map(async (topic) => {
+    const props = convertTopicToForumCellProps(topic)
+    return await props
+  })
 
+  const props = await Promise.all(formattedTopics)
+  return props
+}
 const fetchDiscourse = (url: string) => {
   return fetch(url, {
     headers: {
       "Content-Type": "application/json",
     },
   }).then((response) => response.json())
+}
+
+async function fetchCategoryById(id: number) {
+  const data: CategoryResponse = await fetchDiscourse(
+    [DISCOURSE_API_URL, "c", id.toString(), "show.json"].join("/")
+    // https://docs.discourse.org/#tag/Categories/operation/getCategory
+  )
+
+  const { category } = data
+
+  return category.name
 }
 
 export async function fetchLatestTopics() {
@@ -113,7 +189,9 @@ export async function fetchLatestTopics() {
     topic_list: { topics },
   } = data
 
-  return formatLatestFiveTopics(topics)
+  const formatted = await formatLatestFiveTopics(topics)
+
+  return formatted
 }
 
 // Category: Breaking Changes
