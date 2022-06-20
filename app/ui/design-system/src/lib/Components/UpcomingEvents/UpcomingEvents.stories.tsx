f6e8f49c6f814f54662fd4baceeea673aa4a86d4
Alex Serban <al.se@me.com>
2022-06-20 17:29:05 -0400

    Upcoming events updates (#236)

--- app/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories.tsx
+++ app/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories.tsx
@@ -12,7 +12,7 @@ const Template: Story<UpcomingEventsProps> = (args) => (
   </div>
 )
 
-const events = [
+const upcomingEvents = [
   {
     ctaText: "CTA Here",
     description:
@@ -23,9 +23,10 @@ const events = [
       "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
     location: "Online",
     tags: ["Flow official"],
-    title: "Event Title",
+    title: "Event Title 1",
   },
   {
+    ctaText: "CTA Here",
     href: "https://www.onflow.org",
     description:
       "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
@@ -33,11 +34,24 @@ const events = [
     imageSrc:
       "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
     tags: ["Flow official"],
-    title: "FLIP contest",
+    title: "FLIP contest 2",
     eventDate: "May 5th, 5pm",
+  },
+  {
+    ctaText: "CTA Here",
+    href: "https://www.onflow.org",
+    description:
+      "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+    eventType: "Online",
+    imageSrc:
+      "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+    tags: ["Flow official"],
+    title: "FLIP contest 3 (primary)",
     isPrimary: true,
+    eventDate: "May 5th, 5pm",
   },
   {
+    ctaText: "CTA Here",
     href: "https://www.onflow.org",
     description:
       "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
@@ -45,10 +59,11 @@ const events = [
     imageSrc:
       "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
     tags: ["Flow official"],
-    title: "FLIP contest",
+    title: "FLIP contest 4",
     eventDate: "May 5th, 5pm",
   },
   {
+    ctaText: "CTA Here",
     href: "https://www.onflow.org",
     description:
       "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
@@ -56,10 +71,11 @@ const events = [
     imageSrc:
       "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
     tags: ["Flow official"],
-    title: "FLIP contest",
+    title: "FLIP contest 5",
     eventDate: "May 5th, 5pm",
   },
   {
+    ctaText: "CTA Here",
     href: "https://www.onflow.org",
     description:
       "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
@@ -67,35 +83,66 @@ const events = [
     imageSrc:
       "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
     tags: ["Flow official"],
-    title: "FLIP contest",
+    title: "FLIP contest 6",
     eventDate: "May 5th, 5pm",
   },
+]
+
+const workingHoursEvents = [
   {
-    href: "https://www.onflow.org",
+    ctaText: "CTA Here",
     description:
       "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
-    eventType: "Online",
+    eventDate: "Mar 23",
+    href: "#todo",
     imageSrc:
-      "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61410bc0c8d0522eea319058_Hack-blog_Flow.png",
+      "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+    location: "Online",
     tags: ["Flow official"],
-    title: "FLIP contest",
-    eventDate: "May 5th, 5pm",
+    title: "Upcoming Event Title 1",
+  },
+  {
+    ctaText: "CTA Here",
+    description:
+      "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+    eventDate: "Mar 23",
+    href: "#todo",
+    imageSrc:
+      "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+    location: "Online",
+    tags: ["Flow official"],
+    title: "Upcoming Event Title 2",
+  },
+]
+
+const officeHoursEvents = [
+  {
+    ctaText: "CTA Here",
+    description:
+      "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
+    eventDate: "Mar 23",
+    href: "#todo",
+    imageSrc:
+      "https://assets.website-files.com/5f6294c0c7a8cdf432b1c827/61689102d3325e237fd44b76_unnamed%20(8).png",
+    location: "Online",
+    tags: ["Flow official"],
+    title: "Upcoming Event Title 1",
   },
 ]
 
 const DefaultArgs = {
   goToCommunityHref: "#todo",
   submitEventHref: "#todo",
-  upcoming: events,
-  workingHours: events,
-  officeHours: events,
+  upcoming: upcomingEvents,
+  workingHours: workingHoursEvents,
+  officeHours: officeHoursEvents,
 }
 
 export const Default = Template.bind({})
 Default.args = DefaultArgs
 
 export const SingleEvent = Template.bind({})
-const singleEvent = events.slice(0, 1)
+const singleEvent = upcomingEvents.slice(0, 1)
 
 SingleEvent.args = {
   ...Default.args,
