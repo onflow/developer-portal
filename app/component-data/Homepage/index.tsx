30edd3e1886d5ca473287041ae7c4e3899b7f791
Mackenzie <remember.mackenzie@gmail.com>
2022-06-20 15:43:31 -0700

    Changes route from  'builders' to 'getting-started' (#239)

--- app/component-data/Homepage/index.tsx
+++ app/component-data/Homepage/index.tsx
@@ -9,8 +9,8 @@ import {
 const homepageStartProjectData: LinkCard2ColumnProps = {
   buttonText: "Get started",
   buttonUrl: "/getting-started",
-  description: `Building on Flow is easy.
-      Start building now on web3 with the power of resource-oriented programming, multi-node architecture,
+  description: `Building on Flow is easy. 
+      Start building now on web3 with the power of resource-oriented programming, multi-node architecture, 
       and a host of other features and tooling to make your dapp development easy and efficient.`,
   title: "Start Your Project",
   tags: ["onflow"],
