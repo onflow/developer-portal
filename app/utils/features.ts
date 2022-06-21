1087e370e1a036c0d18adf180c2d2723df05b7a7
Peter Schilling <peter@tanookilabs.com>
2022-06-21 11:25:44 -0700

    Add suggested links to coming soon page (#268)
    Related to #260

--- app/utils/features.ts
+++ app/utils/features.ts
@@ -4,9 +4,11 @@ import { redirect } from "@remix-run/node"
  * @see https://github.com/onflow/next-docs-v1/issues/260
  */
 export function temporarilyRedirectToComingSoon() {
-  // alternatively: allow these pages to load in dev
-  // if (process.env.NODE_ENV === "production") {
-  //   throw redirect(`/coming-soon`)
-  // }
-  throw redirect(`/coming-soon`)
+  const isPreview =
+    process.env.INCOMPLETE_PAGE_BEHAVIOR === "preview" || // allow envs like staging to preview incomplete pages
+    process.env.NODE_ENV === "development" // assume people want to see these pages in dev
+
+  if (!isPreview) {
+    throw redirect(`/coming-soon`)
+  }
 }
