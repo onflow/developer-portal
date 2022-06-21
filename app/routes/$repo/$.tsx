5aa8b13501b96ecd6c4f690620832ca122d1b2fa
Mackenzie Kieran <remember.mackenzie@gmail.com>
2022-06-21 15:06:41 -0700

    Refactor.

--- app/routes/$repo/$.tsx
+++ app/routes/$repo/$.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
 import { LoaderFunction } from "@remix-run/node"
 import { getMdxPage, useMdxComponent } from "~/cms/utils/mdx"
 import { json } from "@remix-run/node"
 import { Link, useCatch, useLoaderData, useLocation } from "@remix-run/react"
 import invariant from "tiny-invariant"
 import { ErrorPage } from "~/ui/design-system/src/lib/Components/ErrorPage"
-import { InternalToc } from "~/ui/design-system/src/lib/Components/InternalToc"
 
 export const loader: LoaderFunction = async ({ params, request }) => {
   const [repo, fileOrDirPath] = [params["repo"], params["*"] || "index"]
@@ -21,7 +19,7 @@ export const loader: LoaderFunction = async ({ params, request }) => {
       { request, forceFresh: process.env.FORCE_REFRESH === "true" }
     )
   } catch (e) {
-    throw json({ status: "mdxError", error: e }, { status: 500 })
+    throw json({ status: "noPage" }, { status: 500 })
   }
 
   if (!page) {
@@ -35,30 +33,12 @@ export const loader: LoaderFunction = async ({ params, request }) => {
 
 export default function () {
   const data = useLoaderData()
-  const location = useLocation()
-  const { code, frontmatter, toc } = data.page
-  const MDXContent = useMdxComponent({ code, frontmatter })
-
-  const [currentHash, setHash] = useState("")
-
-  useEffect(() => {
-    setHash(location.hash)
-  }, [location.hash])
+  const { code, frontmatter } = data.page
+  const Component = useMdxComponent({ code, frontmatter })
 
   return (
-    <div className="pl-[55px]">
-      <div className="flex">
-        <div className="w-[80%] flex-col">
-          <MDXContent />
-        </div>
-        <div className="pt-[55px]">
-          <InternalToc
-            headings={toc}
-            currentHash={currentHash}
-            updateHash={(e) => setHash("#test")}
-          />
-        </div>
-      </div>
+    <div className="flex flex-col py-8 md:flex-row">
+      <Component />
     </div>
   )
 }
@@ -67,44 +47,31 @@ export function CatchBoundary() {
   const caught = useCatch()
   console.error("CatchBoundary", caught)
   const location = useLocation()
-
-  switch (caught.data.status) {
-    case "noPage":
-      return (
-        <ErrorPage
-          title={"404 – Page not found"}
-          subtitle={`there is no page at "${location.pathname}"`}
-          actions={
-            <Link className="underline" to="/">
-              Go home
-            </Link>
-          }
-        />
-      )
-    case "mdxError":
-      return (
-        <ErrorPage
-          title={"Error processing"}
-          subtitle={`An error occured processing the mdx for this document`}
-          actions={
-            <Link className="underline" to="/">
-              Go home
-            </Link>
-          }
-        />
-      )
-    case "noRepo":
-      return (
-        <ErrorPage
-          title={"404 – Repo not found"}
-          subtitle={`This repo is not available or does not exist`}
-          actions={
-            <Link className="underline" to="/">
-              Go home
-            </Link>
-          }
-        />
-      )
+  if (caught.data.status === "noPage") {
+    return (
+      <ErrorPage
+        title={"404 – Page not found"}
+        subtitle={`there is no page at "${location.pathname}"`}
+        actions={
+          <Link className="underline" to="/">
+            Go home
+          </Link>
+        }
+      />
+    )
+  }
+  if (caught.data.status === "noRepo") {
+    return (
+      <ErrorPage
+        title={"404 – Repo not found"}
+        subtitle={`This repo is not available or does not exist`}
+        actions={
+          <Link className="underline" to="/">
+            Go home
+          </Link>
+        }
+      />
+    )
   }
 
   throw new Error(`Unhandled error: ${caught.status}`)
