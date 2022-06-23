fe132e195e033ae9368bb0a97fb7bc793e45743a
Pratik Patel <prpatel05@gmail.com>
2022-06-23 14:53:08 -0400

    Add Test Case For Non Standard NFT.

--- apps/nft-metadata-e2e/src/integration/verifier.spec.ts
+++ apps/nft-metadata-e2e/src/integration/verifier.spec.ts
@@ -14,7 +14,7 @@ describe('nft-metadata/verifier', () => {
     cy.get('#default-search').click();
     cy.get('#default-search').type('0x37d92dad2356b641');
     cy.get('.right-2\\.5').click();
-    cy.get('.no-underline').click();
+    cy.get(':nth-child(8) > .no-underline').click();
     // On step 2
     cy.get(':nth-child(2) > .group').should(
       'have.class',
@@ -29,9 +29,35 @@ describe('nft-metadata/verifier', () => {
       'have.class',
       'border-primary-purple'
     );
-    cy.get('.px-4 > :nth-child(2) > :nth-child(2)').click();
-    cy.get(':nth-child(n) > #headingOne > .relative > svg > circle')
-      .invoke('attr', 'fill')
-      .should('eq', 'green');
+    cy.contains('Continue');
+  });
+
+  it('should go through the verifier flow for non standard contract', () => {
+    // On step 1
+    cy.get(':nth-child(1) > .group').should(
+      'have.class',
+      'border-primary-purple'
+    );
+    cy.get('#default-search').click();
+    cy.get('#default-search').type('0x37d92dad2356b641');
+    cy.get('.right-2\\.5').click();
+    cy.get(':nth-child(9) > .no-underline').click();
+    // On step 2
+    cy.get(':nth-child(2) > .group').should(
+      'have.class',
+      'border-primary-purple'
+    );
+    cy.get('form > :nth-child(4)').click();
+    cy.get('[placeholder="e.g. 0x123456abcdef"]').click();
+    cy.get('[placeholder="e.g. 0x123456abcdef"]').type('0x2aa77a0776d7c209');
+    cy.get('.bg-white').click();
+    // On step 3
+    cy.get(':nth-child(3) > .group').should(
+      'have.class',
+      'border-primary-purple'
+    );
+    cy.contains(
+      'You have not implemented all recommended metadata views required to be added to the NFT catalog.'
+    );
   });
 });
