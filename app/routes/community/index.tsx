68f3b1db0f87f0d7d298ed2e3d113c773e7627aa
Alex Serban <al.se@me.com>
2022-06-23 09:59:26 -0400

    Add selected event state and use component-data events

--- app/routes/community/index.tsx
+++ app/routes/community/index.tsx
@@ -3,11 +3,10 @@ import { useLoaderData } from "@remix-run/react"
 import { LoaderFunction } from "@remix-run/server-runtime"
 import { fetchLatestTopics } from "~/cms/utils/fetch-discourse-api"
 import { fetchFlips } from "~/cms/utils/fetch-flips"
+import { allEvents } from "~/component-data/Events"
 import { getMetaTitle } from "~/root"
 import { CommunityMembersProps } from "~/ui/design-system/src/lib/Components/CommunityMembers"
 import { Default as DefaultCommunityMembers } from "~/ui/design-system/src/lib/Components/CommunityMembers/CommunityMembers.stories"
-import { UpcomingEventsProps } from "~/ui/design-system/src/lib/Components/UpcomingEvents"
-import { Default as DefaultUpcomingEvents } from "~/ui/design-system/src/lib/Components/UpcomingEvents/UpcomingEvents.stories"
 import CommunityPage, {
   CommunityPageProps,
 } from "~/ui/design-system/src/lib/Pages/CommunityPage"
@@ -31,7 +30,7 @@ export const meta: MetaFunction = () => ({
 export const loader: LoaderFunction = async () => {
   temporarilyRedirectToComingSoon()
   const { openFlips, goodPlacesToStartFlips } = await fetchFlips()
-  const upcomingEvents = DefaultUpcomingEvents?.args as UpcomingEventsProps
+  const upcomingEvents = allEvents
   const forumTopics = await fetchLatestTopics()
   const communityMembers =
     DefaultCommunityMembers?.args as CommunityMembersProps
--- app/ui/design-system/src/lib/Components/UpcomingEvents/index.tsx
+++ app/ui/design-system/src/lib/Components/UpcomingEvents/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
+import { OFFICE_HOURS_EVENT_TYPE } from "~/component-data/Events"
 import { ButtonLink } from "../Button"
 import { EventCard, EventCardList, EventCardProps } from "../EventCard"
 import { EventCardSmall } from "../EventCardSmall"
@@ -9,32 +10,33 @@ export type UpcomingEventsProps = {
   events: EventCardProps[]
 }
 
+const FILTERS = [OFFICE_HOURS_EVENT_TYPE]
+
 export function UpcomingEvents({
   goToCommunityHref,
   events,
 }: UpcomingEventsProps) {
-  const [allEvents, setAllEvents] = useState(events)
-  const [selectedEvent, setSelectedEvent] = useState(allEvents[0])
-  console.log(selectedEvent)
-  const primaryEvent =
-    allEvents.filter((event) => event.title === selectedEvent.title)[0] ||
-    allEvents[0]
-  const remainingEvents = allEvents.filter(
-    (event) => event.title !== primaryEvent.title
+  const [tabIndex, setTabIndex] = useState(0)
+  const [selectedEventTitle, setSelectedEventTitle] = useState<string | null>(
+    events[0].title
   )
-  const filterMapping = ["Flow office hours"]
-
   const onTabChange = (filterIndex: number) => {
-    if (filterIndex === 0) {
-      setAllEvents(events)
-      return
-    }
-
-    const filteredEvents = allEvents.filter(
-      (event) => event.eventType === filterMapping[filterIndex - 1]
-    )
-    setAllEvents(filteredEvents)
+    setSelectedEventTitle(null)
+    setTabIndex(filterIndex)
   }
+
+  const filteredEvents = useMemo(
+    () =>
+      tabIndex === 0
+        ? events
+        : events.filter((event) => event.eventType === FILTERS[tabIndex - 1]),
+    [events, tabIndex]
+  )
+  const primaryEvent = useMemo(
+    () =>
+      events.find((e) => e.title === selectedEventTitle) || filteredEvents[0],
+    [events, filteredEvents, selectedEventTitle]
+  )
 
   return (
     <div className="container">
@@ -50,15 +52,19 @@ export function UpcomingEvents({
         <div className="hidden md:block">
           <EventCard {...primaryEvent} className="mb-4" />
           <ul className="hidden list-none flex-row gap-6 overflow-x-auto md:flex">
-            {remainingEvents.map((event: EventCardProps, index: number) => (
-              <li key={index} onClick={() => setSelectedEvent(event)}>
-                <EventCardSmall {...event} />
+            {filteredEvents.map((event: EventCardProps, index: number) => (
+              <li key={index}>
+                <EventCardSmall
+                  {...event}
+                  selected={primaryEvent?.title === event.title}
+                  onClick={() => setSelectedEventTitle(event.title)}
+                />
               </li>
             ))}
           </ul>
         </div>
         <EventCardList
-          events={events}
+          events={filteredEvents}
           breakpoint="none"
           className="visible mb-4 md:hidden"
           carouselItemWidth="w-10/12 md:w-full"
