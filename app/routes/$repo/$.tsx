1de8bd1cd85621ce97574237c85af747508cd8d1
Peter Schilling <peter@tanookilabs.com>
2022-06-24 13:17:47 -0700

    Use consistent top padding above documents

--- app/routes/$repo/$.tsx
+++ app/routes/$repo/$.tsx
@@ -55,7 +55,7 @@ export default function RepoDocument() {
   return (
     <div className="pl-[55px]">
       <div className="grid grid-cols-5">
-        <div className="col-span-4">
+        <div className="col-span-4 pt-8">
           <MDXContent />
         </div>
         <div className="pt-[55px]">
