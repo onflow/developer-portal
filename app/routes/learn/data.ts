212ff65c8925798c7208628a82e06a9cd6729e38
Abhinav Chanda <abhinav.chanda99@gmail.com>
2022-06-24 23:13:25 -0400

    Fixed warnings

--- app/routes/learn/data.ts
+++ app/routes/learn/data.ts
@@ -88,6 +88,6 @@ export const data = {
   cadenceTutorials,
   allTutorials,
   youtubeHref: "https://www.youtube.com/c/FlowBlockchain",
-  cadenceHref: "https://example.com",
+  cadenceHref: "https://docs.onflow.org/cadence/",
   architectureTutorials,
 }
--- app/routes/learn/index.tsx
+++ app/routes/learn/index.tsx
@@ -1,7 +1,6 @@
 import { LoaderFunction, MetaFunction } from "@remix-run/node"
 import { getMetaTitle } from "~/root"
 import { LearnPage } from "~/ui/design-system/src/lib/Pages/LearnPage"
-import { temporarilyRedirectToComingSoon } from "~/utils/features"
 import { data } from "./data"
 
 export const meta: MetaFunction = () => ({
