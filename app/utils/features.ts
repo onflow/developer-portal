5ef51aa698e8d7bf151414869afcb6b94f9f6cda
Hannah Lee <hannah6249@gmail.com>
2022-06-26 22:17:30 -0400

    cleanup code

--- app/utils/features.ts
+++ app/utils/features.ts
@@ -1,11 +1,10 @@
 import { redirect } from "@remix-run/node"
-import { getRequiredGlobalEnvVar } from "~/cms/helpers"
 
 /**
  * @see https://github.com/onflow/next-docs-v1/issues/260
  */
 export function temporarilyRedirectToComingSoon() {
-  const isPreview = process.env.INCOMPLETE_PAGE_BEHAVIOR === "preview"
+  const isPreview = process.env.INCOMPLETE_PAGE_BEHAVIOR === "preview" // allow envs like staging to preview incomplete pages
 
   if (!isPreview) {
     throw redirect(`/coming-soon`)
