7809f9a789bb1e82f26de649087d4099aeb22987
Hannah Lee <hannah6249@gmail.com>
2022-06-26 22:12:57 -0400

    undo additional preview checks

--- app/utils/features.ts
+++ app/utils/features.ts
@@ -5,9 +5,7 @@ import { getRequiredGlobalEnvVar } from "~/cms/helpers"
  * @see https://github.com/onflow/next-docs-v1/issues/260
  */
 export function temporarilyRedirectToComingSoon() {
-  const isPreview =
-    process.env.INCOMPLETE_PAGE_BEHAVIOR === "preview" ||
-    getRequiredGlobalEnvVar("NODE_ENV") !== "production" // allow envs like staging to preview incomplete pages
+  const isPreview = process.env.INCOMPLETE_PAGE_BEHAVIOR === "preview"
 
   if (!isPreview) {
     throw redirect(`/coming-soon`)
