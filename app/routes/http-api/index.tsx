63e888c5c1df7f6558540f35dba29686af9aaca0
Cal Perez <aure.entuluva.cal@gmail.com>
2022-06-28 15:12:50 -0400

    Style better

--- app/routes/http-api/index.tsx
+++ app/routes/http-api/index.tsx
@@ -1,5 +1,6 @@
 import { MetaFunction } from "@remix-run/node"
 import { RedocStandalone } from "redoc"
+import { Theme, useTheme } from "~/cms/utils/theme.provider"
 
 export const meta: MetaFunction = () => {
   return {
@@ -10,9 +11,20 @@ export const meta: MetaFunction = () => {
 }
 
 export default function Page() {
+  const [theme] = useTheme()
+  const isDark = theme === Theme.DARK
+
   return (
     <RedocStandalone
       options={{
+        theme: {
+          colors: { text: { primary: isDark ? "white" : "black" } },
+          rightPanel: { backgroundColor: "black" },
+          sidebar: {
+            backgroundColor: isDark ? "black" : "white",
+            textColor: isDark ? "white" : "black",
+          },
+        },
         nativeScrollbars: true,
       }}
       specUrl="https://raw.githubusercontent.com/onflow/flow/master/openapi/access.yaml"
