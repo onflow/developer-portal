9fae8480f1530c2d812c94d4093702a516fbf3ba
Hannah Lee <hannah6249@gmail.com>
2022-07-11 18:38:04 -0400

    Adding more information to the FLIP cards (#439)

--- app/ui/design-system/src/lib/Components/Flips/RoundImage.tsx
+++ app/ui/design-system/src/lib/Components/Flips/RoundImage.tsx
@@ -1,9 +1,10 @@
 export type RoundImageProps = {
   imageUri: string
+  imageCaption: string
   altText: string
 }
 
-const RoundImage = ({ imageUri, altText }: RoundImageProps) => (
+const RoundImage = ({ imageUri, imageCaption, altText }: RoundImageProps) => (
   <div className="w-9">
     <img
       className="rounded-full border-2 border-white"
@@ -12,6 +13,11 @@ const RoundImage = ({ imageUri, altText }: RoundImageProps) => (
       width={40}
       height={40}
     />
+    <div className="flex items-center justify-center">
+      <div className="dark:gray-400 md:leading-1 text-gray-350 whitespace-nowrap text-xs dark:text-gray-400">
+        {imageCaption}
+      </div>
+    </div>
   </div>
 )
 
