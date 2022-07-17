5dffaeddec8686783dc0f8e32de59c5a824a2295
Cal Perez <aure.entuluva.cal@gmail.com>
2022-07-17 19:44:54 -0400

    Index pages

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,5 +1,5 @@
 describe("smoke tests", () => {
-  it("Should load all main pages without error...", () => {
+  it("Should load all main pages without error", () => {
     cy.visit("http://localhost:3000")
 
     cy.get(".text-h1").should("include.text", "Developer Portal")
@@ -10,17 +10,23 @@ describe("smoke tests", () => {
 
     cy.get('img[alt*="Flow Developers"]').parent().click()
 
-    cy.get('a[href*="/tools"]:contains("View all tools")').click({
-      multiple: true,
-    })
+    cy.get('a[href*="/tools"]:contains("View all tools")').click()
     cy.location("pathname").should("eq", "/tools")
     cy.get(".text-h1").should("include.text", "Tools")
 
-    cy.get(
-      'div:contains("All the resources you need to learn and build.")'
-    ).click()
+    cy.get('img[alt*="Flow Developers"]').parent().click()
+
+    cy.get("footer").within(($footer) => {
+      cy.get('a[href*="/learn"]:contains("Learning Resources")').click()
+    })
     cy.location("pathname").should("eq", "/learn")
     cy.get(".text-h1").should("include.text", "Learn")
+
+    cy.get('img[alt*="Flow Developers"]').parent().click()
+
+    cy.get('a[href*="/community"]:contains("Go to Community")').click()
+    cy.location("pathname").should("eq", "/community")
+    cy.get(".text-h1").should("include.text", "Community")
 
     cy.end()
   })
