ce4eca4b4298b641047c95bc7209b191349e71bf
Neilzon Viloria <46465568+neilZon@users.noreply.github.com>
2022-07-18 16:44:19 -0600

    Mixpanel tracking (#457)

---
+++ app/utils/mixpanel.server.ts
@@ -0,0 +1,27 @@
+import axios from "axios"
+import { Body, Contribution } from "~/routes/action/refresh"
+
+/**
+ * Format and send event data for Mixpanel to ingest
+ *
+ * @param eventData Object containing merge event data
+ */
+export const recordRefreshEventInMixpanel = (eventData: Body) => {
+  const mixpanelData = eventData.contributions.map(
+    (contribution: Contribution) => ({
+      event: "Docs Refresh",
+      properties: {
+        token: process.env.MIXPANEL_DOCSITE_PROJECT_TOKEN,
+        distinct_id: contribution.contributor,
+        commitCount: Number(contribution.commitCount),
+        commitSha: eventData.commitSha,
+        repo: eventData.repo,
+        contentPaths: eventData.contentPaths,
+        keys: eventData.keys,
+      },
+    })
+  )
+  axios.post(process.env.MIXPANEL_EVENT_TRACKING_URL as string, mixpanelData, {
+    headers: { Accept: "text/plain", "Content-Type": "application/json" },
+  })
+}
