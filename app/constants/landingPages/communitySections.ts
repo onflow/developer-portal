3e7d5289dabd39ae5ee40be7c1374249ca5ff805
Alex Serban <al.se@me.com>
2022-08-10 17:00:33 -0400

    Improve empty upcoming events states

--- app/constants/landingPages/communitySections.ts
+++ app/constants/landingPages/communitySections.ts
@@ -1,14 +1,20 @@
-export const communitySections = [
-  { title: "Upcoming Events", elementId: "upcoming-events" },
-  { title: "FLIPs", elementId: "flips" },
-  { title: "Featured Initiatives", elementId: "featured-initiatives" },
-  {
-    title: "Tools",
-    elementId: "tools",
-  },
-  { title: "From the Forum", elementId: "from-the-forum" },
-  {
-    title: "Explore More Content",
-    elementId: "explore-more-content",
-  },
-]
+export const getCommunitySections = (hasEvents: boolean) => {
+  let sections = [
+    { title: "FLIPs", elementId: "flips" },
+    { title: "Featured Initiatives", elementId: "featured-initiatives" },
+    {
+      title: "Tools",
+      elementId: "tools",
+    },
+    { title: "From the Forum", elementId: "from-the-forum" },
+    {
+      title: "Explore More Content",
+      elementId: "explore-more-content",
+    },
+  ]
+
+  if (hasEvents)
+    sections.unshift({ title: "Upcoming Events", elementId: "upcoming-events" })
+
+  return sections
+}
