29a6e5a45caba6d2c614e300e33780e7f994024a
Mackenzie <remember.mackenzie@gmail.com>
2022-08-10 19:54:13 -0700

    Revert "Some updates" (#546)
    Revert "Some updates (#543)"
    
    This reverts commit 1c01075e835c4c6c57467bf7ed1c8c7d0208bc89.

--- app/routes/network/data.ts
+++ app/routes/network/data.ts
@@ -1,11 +1,11 @@
 import { Article } from "~/ui/design-system/src/lib/interfaces"
-import DefaultImage from "~/ui/design-system/images/misc/Asset_8@3x.png"
 
 export const featuredArticle: Article = {
-  heading: "Node operators",
+  heading: "Node operator callout",
   description:
-    "View the current nodes in the network and who is running them. You can use Flow Port to delegate or stake to a node.",
+    "Everything you need to start building on Flow verything you need to start building on Flow everything you need to start building on Flow",
   ctaText: "Learn more",
   ctaLink: "https://flow.com",
-  imageUrl: DefaultImage,
+  imageUrl:
+    "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
 }
