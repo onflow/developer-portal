774de98b15a5ac14f4c08e85897a6a8bc70b90d7
Cal <aure.entuluva.cal@gmail.com>
2022-08-11 11:55:47 -0400

    Fetch spork data (#550)
    * Fetch spork data
    
    * Fix linting
    
    * Swap out octokit instance
    
    * reorder imports

--- app/routes/network/$networkName.tsx
+++ app/routes/network/$networkName.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunction, MetaFunction } from "@remix-run/node"
 import { useLoaderData, useParams } from "@remix-run/react"
 import { fetchNetworkStatus } from "~/cms/utils/fetch-network-status"
+import { fetchSporks } from "~/cms/utils/fetch-sporks"
 import { getMetaTitle } from "~/root"
 import NetworkDetailPage, {
   getNetworkNameFromParam,
@@ -10,7 +11,7 @@ import { featuredArticle } from "./data"
 
 type DynamicNetworkDetailPageProps = Pick<
   NetworkDetailPageProps,
-  "networkStatuses"
+  "networkStatuses" | "pastSporks"
 >
 
 export const meta: MetaFunction = ({ params }) => ({
@@ -23,23 +24,36 @@ export const meta: MetaFunction = ({ params }) => ({
 
 export const loader: LoaderFunction = async () => {
   const networkStatuses = await fetchNetworkStatus()
-  const data = { networkStatuses }
+  const { pastSporks } = await fetchSporks()
+
+  const data = { networkStatuses, pastSporks }
   return data
 }
 
 export default function Page() {
-  const { networkStatuses } = useLoaderData<DynamicNetworkDetailPageProps>()
+  const {
+    networkStatuses,
+    // @ts-ignore
+    pastSporks: { mainnet, testnet },
+  } = useLoaderData<DynamicNetworkDetailPageProps>()
   const params = useParams()
 
   if (!params.networkName) throw new Error("Missing network name")
 
   const networkName = params.networkName
+  let sporks = []
+  if (networkName === "flow-testnet") {
+    sporks = testnet
+  } else if (networkName === "flow-mainnet") {
+    sporks = mainnet
+  }
 
   return (
     <NetworkDetailPage
       networkStatuses={networkStatuses}
       featuredArticle={featuredArticle}
       networkName={networkName}
+      pastSporks={sporks}
     />
   )
 }
