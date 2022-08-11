4ccd35c0b7cf892f5ce5f301179bc1bf81d86780
Cal Perez <aure.entuluva.cal@gmail.com>
2022-08-11 08:12:13 -0400

    add network status

--- app/ui/design-system/src/lib/Components/NavigationBar/MobileMenu.tsx
+++ app/ui/design-system/src/lib/Components/NavigationBar/MobileMenu.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { MenuContent } from "./MenuContent"
 import { MobileMenuBackButton } from "./MobileMenuBackButton"
 import { MobileMenuTopLevel } from "./MobileMenuTopLevel"
-import { DropdownMenuItem, MenuItem } from "./types"
+import { DropdownMenu, DropdownMenuItem, LinkMenuItem, MenuItem } from "./types"
 
 export type MobileMenuProps = {
   menuItems: MenuItem[]
@@ -11,10 +11,11 @@ export type MobileMenuProps = {
 export function MobileMenu({ menuItems }: MobileMenuProps) {
   const [selectedIndex, setSelectedIndex] = useState<[number, number]>([-1, -1])
   const selectedMenuItem = menuItems[selectedIndex[0]] as
-    | DropdownMenuItem
+    | MenuItem
+    | DropdownMenu
     | undefined
 
-  if (selectedMenuItem) {
+  if (selectedMenuItem && "tabs" in selectedMenuItem) {
     return (
       <ul className="w-full divide-y divide-primary-gray-100 dark:divide-primary-gray-400">
         <li>
@@ -30,6 +31,28 @@ export function MobileMenu({ menuItems }: MobileMenuProps) {
             className="px-4"
             cards={selectedMenuItem.tabs[selectedIndex[1]]?.cards}
             sections={selectedMenuItem.tabs[selectedIndex[1]]!.sections}
+          />
+        </li>
+      </ul>
+    )
+  }
+
+  if (selectedMenuItem && "cards" in selectedMenuItem) {
+    return (
+      <ul className="w-full divide-y divide-primary-gray-100 dark:divide-primary-gray-400">
+        <li>
+          <MobileMenuBackButton
+            onClick={() => setSelectedIndex([-1, -1])}
+            className="text-lg text-gray-400 dark:text-gray-100"
+          >
+            {selectedMenuItem.title}
+          </MobileMenuBackButton>
+        </li>
+        <li>
+          <MenuContent
+            className="px-4"
+            cards={selectedMenuItem.cards}
+            sections={selectedMenuItem.sections}
           />
         </li>
       </ul>
--- app/ui/design-system/src/lib/Components/NavigationBar/MobileMenuTopLevel.tsx
+++ app/ui/design-system/src/lib/Components/NavigationBar/MobileMenuTopLevel.tsx
@@ -2,10 +2,11 @@ import { MenuItemLink } from "./MenuItemLink"
 import { MobileMenuButton } from "./MobileMenuButton"
 import { isLinkMenuItem, MenuItem } from "./types"
 import { ReactComponent as ChevronRightIcon } from "../../../../images/arrows/chevron-right"
+import NetworkStatus from "../../../../../../routes/poll-network"
 
 export type MobileMenuTopLevelProps = {
   menuItems: MenuItem[]
-  onItemSelected: (index: [number, number]) => void
+  onItemSelected: (indices: [number, number]) => void
 }
 
 export function MobileMenuTopLevel({
@@ -13,30 +14,47 @@ export function MobileMenuTopLevel({
   onItemSelected,
 }: MobileMenuTopLevelProps) {
   return (
-    <ul>
-      {menuItems.map(({ divider, ...menuItem }, index) => (
-        <li key={index} className="py-2">
-          {isLinkMenuItem(menuItem) ? (
-            <MenuItemLink {...menuItem} className="px-4 py-3" />
-          ) : (
+    <div>
+      <ul>
+        {menuItems.map((menuItem, index) => (
+          <li key={index} className="py-2">
             <div>
               <span className="px-4 text-primary-gray-400 dark:text-primary-gray-200">
                 {menuItem.title}
               </span>
               {menuItem.tabs &&
-                menuItem.tabs.map(({ title }: { title: string }, tabIndex) => (
-                  <MobileMenuButton
-                    className="flex w-full justify-between whitespace-nowrap text-left text-lg"
-                    onClick={() => onItemSelected([index, tabIndex])}
-                    key={title}
-                  >
-                    {title} <ChevronRightIcon />
-                  </MobileMenuButton>
-                ))}
+                menuItem.tabs.map(
+                  ({ title }: { title: string }, tabIndex: number) => (
+                    <MobileMenuButton
+                      className="flex w-full justify-between whitespace-nowrap text-left text-lg"
+                      onClick={() => onItemSelected([index, tabIndex])}
+                      key={title}
+                    >
+                      {title} <ChevronRightIcon />
+                    </MobileMenuButton>
+                  )
+                )}
             </div>
-          )}
+          </li>
+        ))}
+        <li
+          className="hover:cursor px-4 py-2"
+          role="button"
+          onClick={() =>
+            onItemSelected([
+              menuItems.findIndex((item) => item.title === "Network"),
+              0,
+            ])
+          }
+        >
+          <NetworkStatus />
         </li>
-      ))}
-    </ul>
+      </ul>
+      <MenuItemLink
+        href="https://flow.com"
+        title="flow.com"
+        className="fixed bottom-0 right-0 px-4 py-3"
+      />
+    </div>
   )
 }
