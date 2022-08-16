6ded6499e838d725f424d51ab25fec9549761d8f
Mackenzie Kieran <remember.mackenzie@gmail.com>
2022-08-16 10:15:57 -0700

    Fix lint error.

--- app/ui/design-system/src/lib/Components/VideoCard/useExtractYoutubeVideoId.ts
+++ app/ui/design-system/src/lib/Components/VideoCard/useExtractYoutubeVideoId.ts
@@ -5,7 +5,9 @@ import React from "react"
  * https://gist.github.com/rodrigoborgesdeoliveira/987683cfbfcc8d800192da1e73adc486?permalink_comment_id=3262368#gistcomment-3262368
  * it seems pretty exhaustive and works for our current use-cases.
  */
+//
 const YOUTUBE_URL_REGEX =
+  // eslint-disable-next-line
   /(?:http:|https:)*?\/\/(?:www\.|)(?:youtube\.com|m\.youtube\.com|youtu\.|youtube-nocookie\.com).*(?:v=|v%3D|v\/|(?:a|p)\/(?:a|u)\/\d.*\/|watch\?|vi(?:=|\/)|\/embed\/|oembed\?|be\/|e\/)([^&?%#\/\n]*)/im
 
 export type ExtractYoutubeVideoIdResult =
