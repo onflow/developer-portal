3b274d727d2ad5a96ec6019b33861cee37f337ea
Joe Krill <joe@joekrill.com>
2022-08-25 10:08:40 -0400

    Update metadata titles

--- app/data/metadata.ts
+++ app/data/metadata.ts
@@ -1,7 +1,7 @@
 export const metadata = {
-  siteTitle: "Welcome to the Flow Developer Portal",
+  siteTitle: "Flow Developer Portal",
   siteDescription: "Your resource for everything you'll need to build in Flow",
-  defaultPageTitle: "Flow Developer Portal",
+  defaultPageTitle: "Flow Blockchain",
   defaultPageImage:
     "https://flow-og-image.vercel.app/**Explore the Flow Developer Portal**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fstorage.googleapis.com%2Fflow-resources%2Fdocumentation-assets%2Fflow-docs.png&widths=auto&heights=350",
   defaultKeywords: "",
