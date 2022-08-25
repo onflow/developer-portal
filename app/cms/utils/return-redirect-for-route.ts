7b6542ebc5a0d965c3e2192e2603ae50d04aa03d
Mackenzie Kieran <remember.mackenzie@gmail.com>
2022-08-25 12:04:26 -0700

    Remove extensions from github links in markdown.

--- app/cms/utils/return-redirect-for-route.ts
+++ app/cms/utils/return-redirect-for-route.ts
@@ -1,5 +1,9 @@
 import { redirects } from "../../data/redirects"
 
 export function returnRedirectForRoute(path: string) {
+  const hashPath = path.split("#")[1]
+  if (hashPath) {
+    return redirects[path] ? redirects[path] + "#" + hashPath : redirects[path]
+  }
   return redirects[path]
 }
