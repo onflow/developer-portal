fac3d114995d790bbb13856b92b991743b4c331d
Cal <aure.entuluva.cal@gmail.com>
2022-08-31 12:51:55 -0400

    Add padding (#629)
    Closes #608

--- app/ui/design-system/src/lib/Components/LowerPageNav/index.tsx
+++ app/ui/design-system/src/lib/Components/LowerPageNav/index.tsx
@@ -12,7 +12,7 @@ export type LowerPageNavProps = {
 
 export function LowerPageNav({ prev, next }: LowerPageNavProps) {
   return (
-    <div className="flex flex-1 flex-wrap place-content-between">
+    <div className="flex flex-1 flex-wrap place-content-between pt-20">
       {prev ? <LowerPageNavLink link={prev} /> : <span />}
       {next ? <LowerPageNavLink link={next} next={true} /> : <span />}
     </div>
