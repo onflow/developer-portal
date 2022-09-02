1038fb24090bfeb4a658015f7a6e78dc9574c1d4
Joe Krill <joe@joekrill.com>
2022-09-02 11:37:18 -0400

    is not always present

--- app/utils/mixpanel.server.ts
+++ app/utils/mixpanel.server.ts
@@ -7,20 +7,31 @@ import { Body, Contribution } from "~/routes/action/refresh"
  * @param eventData Object containing merge event data
  */
 export const recordRefreshEventInMixpanel = (eventData: Body) => {
-  const mixpanelData = eventData.contributions.map(
+  const properties = {
+    token: process.env.MIXPANEL_DOCSITE_PROJECT_TOKEN,
+    commitCount: 0,
+    commitSha: eventData.commitSha,
+    repo: eventData.repo,
+    contentPaths: eventData.contentPaths,
+    keys: eventData.keys,
+  }
+
+  const mixpanelData = eventData.contributions?.map(
     (contribution: Contribution) => ({
       event: "Docs Refresh",
       properties: {
-        token: process.env.MIXPANEL_DOCSITE_PROJECT_TOKEN,
+        ...properties,
         distinct_id: contribution.contributor,
         commitCount: Number(contribution.commitCount),
-        commitSha: eventData.commitSha,
-        repo: eventData.repo,
-        contentPaths: eventData.contentPaths,
-        keys: eventData.keys,
       },
     })
-  )
+  ) || [
+    {
+      event: "Docs Refresh",
+      properties,
+    },
+  ]
+
   axios.post(process.env.MIXPANEL_EVENT_TRACKING_URL as string, mixpanelData, {
     headers: { Accept: "text/plain", "Content-Type": "application/json" },
   })
