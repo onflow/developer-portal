84ad8bf90e87c426e4d292918f093dcb7acd99cb
10thfloor <remember.mackenzie@gmail.com>
2022-09-13 10:56:15 -0700

    Update robots.txt to disallow crawlers on staging.

--- app/routes/robots[.]txt.ts
+++ app/routes/robots[.]txt.ts
@@ -2,13 +2,26 @@ import { ORIGIN } from "../utils/env.server"
 
 export const loader = () => {
   const sitemapUrl = new URL(`/sitemap.xml`, ORIGIN)
-  const robotsTxt = `# Algolia-Crawler-Verif: 98E1096D4FD67E70
+
+  let robotsTxt = `# Algolia-Crawler-Verif: 98E1096D4FD67E70
 
 User-agent: *
 Sitemap: ${sitemapUrl}
 Disallow: /_meta
 Disallow: /action
 `
+
+  if (process.env.NODE_ENV !== "production") {
+    let robotsTxt = `# Algolia-Crawler-Verif: 98E1096D4FD67E70
+
+  User-agent: Algolia Crawler
+  Sitemap: ${sitemapUrl}
+  Allow: /
+
+  User-agent: *
+  Disallow: /
+  `
+  }
 
   return new Response(robotsTxt, {
     status: 200,
