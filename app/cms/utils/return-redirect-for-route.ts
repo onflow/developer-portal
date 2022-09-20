d60f2e822416a347bdb9d9167c0d02a5d4c00ce7
10thfloor <remember.mackenzie@gmail.com>
2022-09-20 13:42:55 -0700

    Add logging when determining redirect routes.

--- app/cms/utils/return-redirect-for-route.ts
+++ app/cms/utils/return-redirect-for-route.ts
@@ -1,9 +1,16 @@
 import { redirects } from "../../data/redirects"
 
 export function returnRedirectForRoute(path: string) {
+  console.log("Searching for redirect...")
   const hashPath = path.split("#")[1]
   if (hashPath) {
-    return redirects[path] ? redirects[path] + "#" + hashPath : redirects[path]
+    console.log("Request includes hash...", hashPath, path)
+    const result = redirects[path]
+      ? redirects[path] + "#" + hashPath
+      : redirects[path]
+    console.log("Got result:", result)
+    return result
   }
+  console.log("Got result:", redirects[path])
   return redirects[path]
 }
