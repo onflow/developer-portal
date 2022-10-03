524e5445e379e7a1c2c22a1281b37cb262d8b5cc
10thfloor <remember.mackenzie@gmail.com>
2022-10-03 16:00:45 -0700

    Update Mixpanel tracking to work with new webhook endpoint.

--- app/cms/github/webhook-invalidate-cache.ts
+++ app/cms/github/webhook-invalidate-cache.ts
@@ -1,13 +1,33 @@
 import { EmitterWebhookEvent } from "@octokit/webhooks"
 import logger from "../../utils/logging.server"
 import { pushEventCacheKeysToInvalidate } from "../github-webhook.server"
+import { recordRefreshEventInMixpanel } from "~/utils/mixpanel.server"
 import { del } from "../redis.server"
+import { commitShaKey } from "~/routes/action/refresh"
 
 export const invalidateCacheOnPush = (event: EmitterWebhookEvent<"push">) => {
   const { cacheKeysToInvalidate } = pushEventCacheKeysToInvalidate(
     event.payload
   )
   const keyCount = cacheKeysToInvalidate.size
+
+  const { pusher, ref, repository } = event.payload
+
+  const allChangedFiles = event.payload.commits.flatMap((commit) => [
+    ...commit.added,
+    ...commit.removed,
+    ...commit.modified,
+  ])
+
+  recordRefreshEventInMixpanel({
+    pusher: pusher.username!,
+    ref,
+    repo: {
+      name: repository.name,
+    },
+    updatedFiles: allChangedFiles,
+  })
+
   if (keyCount > 0) {
     logger.info(
       `Github webhook: clearing cache keys ${[...cacheKeysToInvalidate].join(
--- app/utils/mixpanel.server.ts
+++ app/utils/mixpanel.server.ts
@@ -2,39 +2,22 @@ import { Body, Contribution } from "~/routes/action/refresh"
 import mixpanel from "mixpanel"
 import { getRequiredServerEnvVar } from "~/cms/helpers"
 
-const mpTokenInvalid = "idk" // <-- This should be the value set for the MIXPANEL_DOCSITE_PROJECT_TOKEN
-// env var in environments where we want to disable tracking.
-
 const mpToken = getRequiredServerEnvVar("MIXPANEL_DOCSITE_PROJECT_TOKEN")
-mixpanel.init(mpToken, { debug: true })
+
 /**
  * Format and send event data for Mixpanel to ingest
  *
  * @param eventData Object containing merge event data
  */
-export const recordRefreshEventInMixpanel = (eventData: Body) => {
-  const properties = {
-    commitCount: 0,
-    commitSha: eventData.commitSha,
-    repo: eventData.repo,
-    contentPaths: eventData.contentPaths,
-    keys: eventData.keys,
-  }
-
-  const mixpanelData = eventData.contributions?.map(
-    (contribution: Contribution) => ({
-      ...properties,
-      distinct_id: contribution.contributor,
-      commitCount: Number(contribution.commitCount),
-    })
-  ) || [
-    {
-      properties,
-    },
-  ]
-
+export const recordRefreshEventInMixpanel = (eventData: {
+  pusher: string
+  ref: string
+  repo: { name: string }
+  updatedFiles: string[]
+}) => {
+  mixpanel.init(mpToken, { debug: true })
   console.log("Got contribution event")
 
   console.log("Sending data to Mixpanel:")
-  mixpanel.track("documents_updated", mixpanelData)
+  mixpanel.track("documents_updated", eventData)
 }
