09d21c222011c79aec2d21e4ac51768245a40446
Joe Krill <joe@joekrill.com>
2022-10-03 15:35:24 -0400

    Add hints for invalid links

--- app/cms/doc-validation/validate-collection.server.ts
+++ app/cms/doc-validation/validate-collection.server.ts
@@ -15,6 +15,10 @@ const PLACEHOLDER_ORIGIN = "https://example.com"
 const normalizeRelativeUrl = (path: string) =>
   stripSlahes(stripMarkdownExtension(path.toLowerCase()))
 
+export type InvalidLinkItem = LinkItem & { hint?: string }
+
+type TestableLinkItem = LinkItem & { normalized: string; isValid: boolean }
+
 export type FileValidationResult =
   | {
       file: string
@@ -29,7 +33,7 @@ export type FileValidationResult =
   | {
       file: string
       status: "link-error"
-      invalidLinks: LinkItem[]
+      invalidLinks: InvalidLinkItem[]
     }
   | {
       file: string
@@ -113,17 +117,31 @@ export const validateCollection = async (
       .filter(({ href }) => !href.toLowerCase().startsWith("mailto:"))
       // Ignore hash links for now - we'll add support for these later.
       .filter(({ href }) => !href.startsWith("#"))
-      .filter(({ href }) => {
+      .map<TestableLinkItem>((link) => {
         // This ensures we strip out any query strings or hashes (we can
         // verify hashes another time)
-        const { pathname } = new URL(href, PLACEHOLDER_ORIGIN)
+        const { pathname } = new URL(link.href, PLACEHOLDER_ORIGIN)
 
         // resolve the path relative to the file's root path, but excluding
         // the source's root path (which we cannot "break out" of)
         const resolved = posix.resolve("/", rootRelativePath, pathname)
+        const normalized = normalizeRelativeUrl(resolved)
 
-        return !relativeFileUrls.includes(normalizeRelativeUrl(resolved))
-      }) as LinkItem[]
+        return {
+          ...link,
+          normalized,
+          isValid: relativeFileUrls.includes(normalizeRelativeUrl(resolved)),
+        }
+      })
+      .filter((link) => !link.isValid)
+      .map<InvalidLinkItem>((link) => ({
+        href: link.href,
+        position: link.position,
+        hint: getInvalidLinkHint(link, {
+          localFileUrls: relativeFileUrls,
+          collection,
+        }),
+      }))
 
     if (invalidLinks.length > 0) {
       return {
@@ -138,4 +156,30 @@ export const validateCollection = async (
       status: "ok",
     }
   })
+}
+
+type GetInvalidLinkHintOptions = {
+  localFileUrls: string[]
+  collection: DocCollection
+}
+
+/**
+ * Tries to make an educated guess about what the user was trying to link to and/or offer some additional guidance about how to correct the link.
+ */
+function getInvalidLinkHint(
+  { href }: TestableLinkItem,
+  { localFileUrls, collection }: GetInvalidLinkHintOptions
+): string | undefined {
+  const possibleUrl = localFileUrls.find((url) => href.includes(url))
+  if (possibleUrl) {
+    return `Did you mean \`${possibleUrl}\`?`
+  }
+
+  if (href.startsWith("/")) {
+    const { rootPath } = collection.source
+    const strippedHref = stripSlahes(href)
+    return `This looks like an absolute path. If you are linking to a relative file in the same doc collection (within \`${rootPath}\`) then you should use a relative path (Maybe you meant \`${strippedHref}\`?). If you're referencing a link external to this doc collection (outside of \`${rootPath}\`) you should use an absolute URL (Maybe you meant \`https://developers.flow.com/${strippedHref}\`?)`
+  }
+
+  return undefined
 }
