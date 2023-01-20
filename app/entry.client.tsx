5fe49adfa4971787d4675468559e9ae906dc08b0
Daniel Cheng <dcheng47@gmail.com>
2023-01-19 17:43:08 -0800

    removed process.env.HOTJAR_SITE_ID

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,7 +2,11 @@ import { RemixBrowser } from "@remix-run/react"
 import { hydrate } from "react-dom"
 import { onCLS, onFID, onLCP, onFCP, onINP, onTTFB } from "web-vitals"
 import { reportWebVitalsToGA } from "./utils/gtags.client"
+import Hotjar from "@hotjar/browser"
 
+const hotjarVersion = 6
+const hotjarsiteid = 3324806
+Hotjar.init(hotjarsiteid, hotjarVersion)
 // TODO: Figure out why this is necessary only for these 3 images.
 // Possibly related to: https://github.com/remix-run/remix/issues/3414
 //
