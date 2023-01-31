8ba7f5820cbe900f3c859c9aacdb0232146ebf64
Tom Haile <bradford.haile@gmail.com>
2023-01-31 11:39:36 -0600

    fix reference to mobile landing image

--- app/ui/design-system/src/lib/Components/InternalLandingHeader/icons.ts
+++ app/ui/design-system/src/lib/Components/InternalLandingHeader/icons.ts
@@ -1,6 +1,6 @@
 import { ReactComponent as CadenceLandingIcon } from "../../../../images/tools/tool-cadence-landing"
 import { ReactComponent as FclIcon } from "../../../../images/tools/tool-fcl"
-import { ReactComponent as MobileIcon } from "../../../../images/tools/tool-mobile"
+import { ReactComponent as MobileIcon } from "../../../../images/tools/tool-mobile-landing"
 
 export const LANDING_HEADER_ICONS = {
   cadence: CadenceLandingIcon,
