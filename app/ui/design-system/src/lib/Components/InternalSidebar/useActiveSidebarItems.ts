6b0a15b0c5d34c58c1604fb1feb09fb1c068f6ad
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-03-03 14:43:37 -0500

    :bug: Hide external links from pagination

--- app/ui/design-system/src/lib/Components/InternalSidebar/useActiveSidebarItems.ts
+++ app/ui/design-system/src/lib/Components/InternalSidebar/useActiveSidebarItems.ts
@@ -5,6 +5,7 @@ import { stripTrailingSlashes } from "../../utils/stripTrailingSlashes"
 import { titleFromHref } from "../../utils/titleFromHref"
 import { resolveUrl } from "../../utils/useResolvedUrl"
 import { InternalSidebarUrlContext } from "./InternalSidebarUrlContext"
+import { isLinkExternal } from "../../utils/isLinkExternal"
 
 /**
  * Flattens a nested array of `SidebarItem`s
@@ -16,7 +17,10 @@ export const flattenItems = (items?: SidebarItem[]): SidebarItem[] =>
  * Finds the current active sidebar item, and the previous and next items
  * relative to the active item.
  */
-export const useActiveSidebarItems = (items: SidebarItem[]) => {
+export const useActiveSidebarItems = (
+  items: SidebarItem[],
+  hideExternal: boolean = false
+) => {
   const location = useLocation()
   const path = stripTrailingSlashes(location.pathname)
   const { basePath, searchParams } = useContext(InternalSidebarUrlContext)
@@ -24,11 +28,20 @@ export const useActiveSidebarItems = (items: SidebarItem[]) => {
   const linkItems =
     flattenItems(items).filter<SidebarLinkItem>(isSidebarLinkItem)
 
-  const resolvedLinkItems = linkItems.map((item) => ({
-    href: resolveUrl(item.href, basePath, { searchParams }),
-    path: stripTrailingSlashes(`${basePath}${item.href}`),
-    title: item.title || titleFromHref(item.href),
-  }))
+  const resolvedLinkItems = linkItems
+    .map((item) => {
+      if (hideExternal && isLinkExternal(item.href)) {
+        return undefined
+      }
+      return {
+        href: isLinkExternal(item.href)
+          ? item.href
+          : resolveUrl(item.href, basePath, { searchParams }),
+        path: stripTrailingSlashes(`${basePath}${item.href}`),
+        title: item.title || titleFromHref(item.href),
+      }
+    })
+    .filter(Boolean)
 
   const activeIndex = resolvedLinkItems.findIndex(
     (resolved) => resolved.path === path
