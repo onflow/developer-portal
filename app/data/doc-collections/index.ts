5c03a1aeb76ae12f8dcc9a3abb476beb54a25130
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2023-03-21 13:43:34 -0500

    Add back in account-linking for redirects

--- app/data/doc-collections/index.ts
+++ app/data/doc-collections/index.ts
@@ -30,6 +30,7 @@ import tools__flow_go_sdk from "./tools__flow-go-sdk.json"
 import tools__flow_js_testing from "./tools__flow-js-testing.json"
 import tools__vscode_extension from "./tools__vscode-extension.json"
 import tools__unity_sdk from "./tools__unity-sdk.json"
+import account_linking from "./account-linking.json"
 import hybrid_custody from "./hybrid-custody.json"
 import learn from "./learn.json"
 import quickstart from "./quickstart.json"
@@ -82,6 +83,7 @@ export const docCollections = {
   "flow-js-testing": tools__flow_js_testing,
   "vscode-extension": tools__vscode_extension,
   "unity-sdk": tools__unity_sdk,
+  "account-linking": account_linking,
   "hybrid-custody": hybrid_custody,
   learn: learn,
   quickstart: quickstart,
