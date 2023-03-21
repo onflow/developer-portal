ff93f431043ea5091535eb1a5e4d0cbaef48acb5
Tom Haile <bradford.haile@gmail.com>
2023-03-21 15:34:43 -0500

    leave account linking

--- app/data/doc-collections/index.ts
+++ app/data/doc-collections/index.ts
@@ -30,6 +30,7 @@ import tools__flow_go_sdk from "./tools__flow-go-sdk.json"
 import tools__flow_js_testing from "./tools__flow-js-testing.json"
 import tools__vscode_extension from "./tools__vscode-extension.json"
 import tools__unity_sdk from "./tools__unity-sdk.json"
+import account_linking from "./account-linking.json"
 import hybrid_custody from "./hybrid-custody.json"
 import nft_catalog from "./tools__nft-catalog.json"
 
@@ -78,6 +79,7 @@ export const docCollections = {
   "tools/flow-js-testing": tools__flow_js_testing,
   "tools/vscode-extension": tools__vscode_extension,
   "tools/unity-sdk": tools__unity_sdk,
+  "account-linking": account_linking,
   "hybrid-custody": hybrid_custody,
   "tools/nft-catalog": nft_catalog,
 }
